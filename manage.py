9ce0e4c2633074278cad3af01e60e925353d3d81
Will Roscoe <wroscoe@gmail.com>
2016-12-11 00:29:01 -0800

    added image utils and image formatting

--- manage.py
+++ manage.py
@@ -3,24 +3,23 @@ import tty, sys #used for live keyboard input
 import argparse
 from time import sleep
 import curses
-
-from picamera import PiCamera
+import os 
+from PIL import Image
 
 from whip import Whip
+from utils import image as image_utils
 
-parser = argparse.ArgumentParser()
-subparsers = parser.add_subparsers(help='sub-command help', dest="subparser_name")
-
-parser_a = subparsers.add_parser('drive', help='drive help')
-parser_b = subparsers.add_parser('record', help='record help')
-
-args = parser.parse_args()
+from picamera import PiCamera
+camera = PiCamera()
 
 
 BASE_URL = 'http://localhost:8000/'
-PHOTOS_PATH = '/home/wroscoe/.tmp/'
+IMG_DIR =  os.path.expanduser("~") + '/donkey_imgs/'
 
 
+def setup():
+    if not os.path.exists(IMG_DIR):
+        os.makedirs(IMG_DIR)
 
 def drive():
     whip = Whip(BASE_URL)
@@ -28,32 +27,51 @@ def drive():
 
 def record(stdscr):
     ''' record pictures on an interval '''
-    camera = PiCamera()
 
     file_num = 0
 
     while True:
 
-        sleep(1)
-        print('capturing')
-
+        sleep(3)
+        print('capturing picture' + str(file_num))
         file_name = 'donkey_' + str(file_num) + '.jpg'
         file_num += 1
 
-        camera.capture(file_name)
+        # Create the in-memory stream
+        stream = io.BytesIO()
+        with picamera.PiCamera() as camera:
+            camera.resolution = (640, 480)
+            camera.capture(stream, format='jpeg', resize=(320, 240))
+        # "Rewind" the stream to the beginning so we can read its content
+        stream.seek(0)
+        img = Image.open(stream)
+        img = image_utils.square(img)
+        img = image_utils.scale(img, 128)
+
+        img.save(file_name, 'jpeg')
 
 
 
 
+
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers(help='sub-command help', dest="subparser_name")
+
+parser_drive = subparsers.add_parser('drive', help='drive help')
+parser_record = subparsers.add_parser('record', help='record help')
+parser_setup = subparsers.add_parser('setup', help='record help')
+
+args = parser.parse_args()
 
 
 if __name__ == '__main__':
     if 'subparser_name' in args:
         if args.subparser_name == 'drive':
             print('starting donkey in drive mode') 
-            print('press "d" to exit') 
-            curses.wrapper(drive)
+            drive()
         elif args.subparser_name == 'record':
             print('starting donkey in drive mode') 
-            print('press " " to exit') 
-            curses.wrapper(record)
+            record()
+        elif args.subparser_name == 'setup':
+            print('starting donkey in drive mode') 
+            setup()
---
+++ utils/image.py
@@ -0,0 +1,71 @@
+from io import BytesIO
+from PIL import Image
+from skimage import color, transform, util, img_as_float, io, filters, morphology, exposure
+from skimage.restoration import denoise_tv_chambolle
+
+def square(im):
+    ''' 
+    accepts: PIL image
+    returns: the center square of an PIL image
+    '''
+    
+    width, height = im.size
+    size = min(height, width)
+    width_diff = (width-size)/2
+    height_diff = (height-size)/2
+    x1 = width_diff
+    x2 = width - width_diff
+    y1 = height_diff
+    y2 = height - height_diff
+    im = im.crop((x1, y1, x2, y2))
+    return im
+
+
+def scale(im, size=128):
+    '''
+    accepts: PIL image, size of square sides
+    returns: PIL image scaled so sides lenght = size 
+    '''
+    
+    size = (size,size)
+    im.thumbnail(size, Image.ANTIALIAS)
+    return im
+
+
+def img_to_binary(img):
+    '''
+    accepts: PIL image
+    returns: binary stream (used to save to database)
+    '''
+
+    f = BytesIO()
+    img.save(f, format='png')
+    return f.getvalue()
+
+def binary_to_img(binary):
+    '''
+    accepts: binary file object from BytesIO
+    returns: PIL image
+    '''
+    img = BytesIO(binary)
+    return Image.open(img)
+
+
+def arr_preprocess(arr):
+    '''
+    accepts: greyscale image array
+    returns: greyscale immage array that has been denoised and globally equalized.
+    '''
+    arr_denoise = denoise_tv_chambolle(arr, weight=0.03, multichannel=True)
+    arr_global_equalized =  exposure.equalize_hist(arr_denoise)
+    return arr_global_equalized
+
+def arr_edges(arr):
+    '''
+    accepts: greyscale image array
+    returns: greyscale immage array of image edges
+    '''
+    arr_v = filters.prewitt_v(arr)
+    arr_h = filters.prewitt_h(arr)
+    arr_t = arr_v + arr_h
+    return arr_t
