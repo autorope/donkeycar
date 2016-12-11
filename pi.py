c2758420ba074a7e3b3ee49fa291099ad9996001
Will <wroscoe@gmail.com>
2016-12-11 07:01:38 +0000

    not sure

--- pi.py
+++ pi.py
@@ -8,12 +8,26 @@ image_dir = os.getcwd()
 
 path = image_dir + '/donkey.jpg'
 
+from io import BytesIO
+from time import sleep
+from picamera import PiCamera
 
+
+camera = PiCamera()
+
+# Create an in-memory stream
+
+camera.start_preview(alpha=200)
+# Camera warm-up time
+sleep(2)
+camera.stop_preview()
+
+my_stream = BytesIO()
+camera.capture(my_stream, 'jpeg')
+
+
+
+def get_picture():
+    pass
 if __name__ == "__main__":
-    
-
-    url = 'http://localhost:5000/upload'
-    files = {'file': open(path, 'rb')}
-    r = requests.post(url, files=files)
-    #response = requests.get(url)
-    print(r.text)
+        pass
