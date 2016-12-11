cc2b9a3c4b42e384797fbe9f2ada53c2465b7e3b
Will Roscoe <wroscoe@gmail.com>
2016-12-10 22:56:43 -0800

    updated camera

--- manage.py
+++ manage.py
@@ -1,18 +1,69 @@
-from flask.ext.sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-db = SQLAlchemy(app)
+import tty, sys #used for live keyboard input
+import argparse
+from time import sleep
+import curses
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+#from picamera import PiCamera
 
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
+from whip import Whip
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128))
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers(help='sub-command help', dest="subparser_name")
+
+parser_a = subparsers.add_parser('drive', help='drive help')
+parser_b = subparsers.add_parser('record', help='record help')
+
+args = parser.parse_args()
+
+
+BASE_URL = 'http://localhost:8000/'
+PHOTOS_PATH = '/home/wroscoe/.tmp/'
+
+
+
+def drive():
+    whip = Whip(BASE_URL)
+
+
+def record(stdscr):
+    ''' record pictures on an interval '''
+    #camera = PiCamera()
+
+    file_num = 0
+
+    # do not wait for input when calling getch
+    stdscr.nodelay(1)
+    while True:
+        # get keyboard input, returns -1 if none available
+        c = stdscr.getch()
+        if c != -1:
+            # print numeric value
+            stdscr.addstr(str(c) + ' ')
+            stdscr.refresh()
+            # return curser to start position
+            stdscr.move(0, 0)
+
+        sleep(1)
+        print('capturing')
+        #camera.capture()
+
+        file_name = 'donkey_' + str(file_num)
+        file_num += 1
+
+
+
+
+
+
 
 if __name__ == '__main__':
-    manager.run()
+    if 'subparser_name' in args:
+        if args.subparser_name == 'drive':
+            print('starting donkey in drive mode') 
+            print('press "d" to exit') 
+            curses.wrapper(drive)
+        elif args.subparser_name == 'record':
+            print('starting donkey in drive mode') 
+            print('press " " to exit') 
+            curses.wrapper(record)
