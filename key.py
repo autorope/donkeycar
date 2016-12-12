b2d3a30fc6d63a50e5a88a55ecddd68f2344a6eb
Will Roscoe <wroscoe@gmail.com>
2016-12-11 23:46:50 -0800

    fixed keyboard imput to adjust global speed and angles from a thread

--- key.py
+++ key.py
@@ -1,30 +1,90 @@
-from pynput.keyboard import Key, Listener
+'''
+Code to manually set the speed and angle of the vehicle. 
 
+angle: the angle of the wheels ranging between -90 (full left)
+        and 90 (full right)
 
-def on_press(key):
-    print('{0} pressed'.format(
-        key))
+speed: desired speed of the vehicle ranging between 100 (full forward)
+        and -100 full backward
 
-def on_release(key):
-    print('{0} release'.format(
-        key))
-    if key == Key.esc:
-        # Stop listener
-        return False
+Key Commands:
 
-# Collect events until released
+i - speed up
+k - slow down
+j - turn left
+l - turn right
 
-def start_listener():
-  with Listener(
-          on_press=on_press,
-          on_release=on_release) as listener:
-      listener.join()
+TODO: This could be improved by increasing changing angle and speed 
+by the time the key is pressed. There is currently a lag between the 
+first key stroke and sequential ones. 
+
+'''
+
 
 
 import threading
+from time import sleep
+
+from pynput.keyboard import Key, Listener, KeyCode
+
+ANGLE_INTERVAL = 5
+MAX_ANGLE = 90
+
+SPEED_INTERVAL = 5
+MAX_SPEED = 100
 
 
-t = threading.Thread(target=start_listener)
-t.start()
+def on_press(key):
+    global angle
+    global speed
+    global stop
 
-print('hello')
+    if key == KeyCode(char="i"): #speed up
+        speed = min(speed + SPEED_INTERVAL, MAX_SPEED)
+    
+    elif key == KeyCode(char="k"): #slow down speed
+        speed = max(speed - SPEED_INTERVAL, -MAX_SPEED)
+    
+    elif key == KeyCode(char="j"): #angle left
+        angle = max(angle - ANGLE_INTERVAL, -MAX_ANGLE)
+    
+    elif key == KeyCode(char="l"): #angle right
+        angle = min(angle + ANGLE_INTERVAL, MAX_ANGLE)
+
+    elif key == Key.space: #togle stop
+        print('stop pressed')
+        stop = not stop
+
+    elif key == Key.backspace:
+        print('reseting angle and speed to zero')
+        angle = 0
+        speed = 0
+
+
+# Collect events until released
+
+def start_keyboard_listener():
+    with Listener(
+          on_press=on_press) as listener:
+      listener.join()
+
+
+
+
+if __name__ == '__main__':
+
+    '''
+    example how to use
+    '''
+
+    angle = 0
+    speed = 0
+    stop = True
+
+    t = threading.Thread(target=start_keyboard_listener)
+    t.daemon = True #to close thread on Ctrl-c
+    t.start()
+
+    while True:
+        sleep(1)
+        print('angle: %s,  speed: %s' %(angle, speed))
