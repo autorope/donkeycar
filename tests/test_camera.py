4301154ec87c06ccce4181189bed526a1ebaa693
Will Roscoe <wroscoe@gmail.com>
2017-01-01 19:57:21 -0800

    update mapping test

--- tests/test_camera.py
+++ tests/test_camera.py
@@ -18,14 +18,7 @@ class TestStringMethods(unittest.TestCase):
 
 
 
-def map_range(x, X_min, X_max, Y_min, Y_max):
-    X_range = X_max - X_min
-    Y_range = Y_max - Y_min
-    XY_ratio = X_range/Y_range
-
-    y = ((x-X_min) / XY_ratio + Y_min) // 1
-
-    return int(y)
+from donkey.actuators import map_range
 
 
 
