e44bcd9f24c5dff40121647c6e8d28ab47897528
Will Roscoe <wroscoe@gmail.com>
2017-01-01 19:55:49 -0800

    update mapping function

--- donkey/actuators.py
+++ donkey/actuators.py
@@ -17,8 +17,8 @@ def map_range(x, X_min, X_max, Y_min, Y_max):
     X_range = X_max - X_min
     Y_range = Y_max - Y_min
     XY_ratio = X_range/Y_range
-    
-    y = (x / XY_ratio + Y_min + .5 * Y_range) // 1
+
+    y = ((x-X_min) / XY_ratio + Y_min) // 1
 
     return int(y)
 
--- tests/test_camera.py
+++ tests/test_camera.py
@@ -16,5 +16,36 @@ class TestStringMethods(unittest.TestCase):
         with self.assertRaises(TypeError):
             s.split(2)
 
+
+
+def map_range(x, X_min, X_max, Y_min, Y_max):
+    X_range = X_max - X_min
+    Y_range = Y_max - Y_min
+    XY_ratio = X_range/Y_range
+
+    y = ((x-X_min) / XY_ratio + Y_min) // 1
+
+    return int(y)
+
+
+
+class TestMapping(unittest.TestCase):
+    def test_positive(self):
+        min = map_range(-100, -100, 100, 0, 1000)
+        half = map_range(0, -100, 100, 0, 1000)
+        max = map_range(100, -100, 100, 0, 1000)
+        assert min == 0
+        assert half == 500
+        assert max == 1000
+
+    def test_negative(self):
+        ranges = (0, 100, 0, 1000)
+        min = map_range(0, *ranges)
+        half = map_range(50, *ranges)
+        max = map_range(100, *ranges)
+        assert min == 0
+        assert half == 500
+        assert max == 1000
+
 if __name__ == '__main__':
     unittest.main()
