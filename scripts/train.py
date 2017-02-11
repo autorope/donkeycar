eaf638360a5fd542925fa54a99464c4a9bdd31ec
Will Roscoe <wroscoe@gmail.com>
2017-02-10 16:41:25 -0800

    updated training file

--- scripts/train.py
+++ scripts/train.py
@@ -7,18 +7,18 @@ previously recorded session.
 import donkey as dk
 
 #Train on session pictures
-sh = dk.sessions.SessionHandler(sessions_path='~/donkey_data/sessions/')
-s = sh.load('manual_white_line')
-X, Y = s.load_dataset()
+#sh = dk.sessions.SessionHandler(sessions_path='~/donkey_data/sessions/')
+#s = sh.load('manual_white_line')
+#X, Y = s.load_dataset()
 
 
 #Train on simulated pictures
-#X, Y = dk.datasets.moving_square(n_frames=2000, return_x=True, return_y=False)
+X, Y = dk.datasets.moving_square(n_frames=2000, return_x=True, return_y=False)
 
 
-print('Downloading file, this could take some time.)
-url = 'https://s3.amazonaws.com/donkey_resources/port.pkl'
-X, Y = dk.datasets.load_url(url)
+#print('Downloading file, this could take some time.')
+#url = 'https://s3.amazonaws.com/donkey_resources/port.pkl'
+#X, Y = dk.datasets.load_url(url)
 
 print('Loading Model.')
 model = dk.models.cnn3_full1()
