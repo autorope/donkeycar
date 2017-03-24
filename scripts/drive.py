314accc8dddb7e7b087a83422270482e3324516d
Yannis <yconst@gmail.com>
2017-03-24 23:28:00 +0200

    Refactored start-server script to automatically perform most tasks (#74)
    * Refactored start-server script to automatically perform most tasks
    
    * Added config files for some vehicles
    
    * Added multiple vehicle support via command line option

--- scripts/drive.py
+++ scripts/drive.py
@@ -6,10 +6,11 @@ will use post requests to the server specified in the remote argument. Run the
 serve.py script on a different computer to start the remote server.
 
 Usage:
-    drive.py [--remote=<name>] 
+    drive.py [--remote=<name>] [--config=<name>]
 
 Options:
   --remote=<name>   recording session name
+  --config=<name>   vehicle configuration file name (without extension)  [default: vehicle]
 """
 
 import os
@@ -17,13 +18,12 @@ from docopt import docopt
 
 import donkey as dk
 
-
-
 # Get args.
 args = docopt(__doc__)
 
-
 if __name__ == '__main__':
+
+    cfg = dk.config.parse_config('~/mydonkey/' + args['--config'] + '.ini')
 
     #get the url for the remote host (for user control)
     remote_url = args['--remote']
