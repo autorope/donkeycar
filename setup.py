ecee4b2339eca415050d956c656519f27c514c7b
Stas <stanislav.levental@gmail.com>
2017-05-18 16:47:40 -0700

    removed opencv

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(name='donkey',
                         'keras',
                         'h5py',
                         'scikit-image',
-                        'opencv-python',
+                        # 'opencv-python',
                         'pandas'
                         ],
                     'pi': [
