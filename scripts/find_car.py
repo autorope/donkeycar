ca079d54017ac5266a74540011144201b697f421
Will Roscoe <wroscoe@gmail.com>
2017-07-28 21:56:29 -0700

    fix find_car.py script to use users existing ip

--- scripts/find_car.py
+++ scripts/find_car.py
@@ -2,10 +2,7 @@
 Convienience script to find your Raspberry Pi on a local network. 
 
 Usage:
-    find_car.py [--ip=<ip>]
-
-Options:
-  --ip=<ip>   Base ip address of your network [default: 192.168.1.0]
+    find_car.py 
 """
 
 
@@ -14,11 +11,11 @@ import os
 import socket
 
 args = docopt(__doc__)
-ip = args['--ip']
 
 print('Looking up your computer IP address...')
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8",80))
+ip = s.getsockname()[0] 
 print('Your IP address: %s ' %s.getsockname()[0])
 s.close()
 
