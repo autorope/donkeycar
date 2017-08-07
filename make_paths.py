6d9d8f7a57d427f282e2ce5c8e2889238e6d5352
Tawn Kramer <tawnkramer@gmail.com>
2017-08-07 16:59:54 -0700

    fixes

--- make_paths.py
+++ make_paths.py
@@ -10,9 +10,9 @@ def make_path(rel_path):
 
 def copy_file(src, dest):
     dest_path = os.path.expanduser(dest)
-    shutil.copyfile(src, dest)
+    shutil.copyfile(src, dest_path)
         
 make_path('~/mydonkey/datasets')
 make_path('~/mydonkey/models')
-copy_file('config/default.h5', '~/mydonkey/models')
-copy_file('config/vehicle.ini', '~/mydonkey/')
+copy_file('config/default.h5', '~/mydonkey/models/default.h5')
+copy_file('config/vehicle.ini', '~/mydonkey/vehicle.ini')
