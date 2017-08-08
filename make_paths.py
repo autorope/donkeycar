f19485b75098d40a8f29fc06043c7fc21cf85011
Tawn Kramer <tawnkramer@gmail.com>
2017-08-08 10:51:24 -0700

    added sessions to make_paths

--- make_paths.py
+++ make_paths.py
@@ -12,7 +12,9 @@ def copy_file(src, dest):
     dest_path = os.path.expanduser(dest)
     shutil.copyfile(src, dest_path)
         
+make_path('~/mydonkey')
 make_path('~/mydonkey/datasets')
+make_path('~/mydonkey/sessions')
 make_path('~/mydonkey/models')
 copy_file('config/default.h5', '~/mydonkey/models/default.h5')
 copy_file('config/vehicle.ini', '~/mydonkey/vehicle.ini')
