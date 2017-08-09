d023e6b44aa794063fe052db5a5a8f056ce4a910
Will Roscoe <wroscoe@gmail.com>
2017-08-08 19:22:24 -0700

    refactored actuators to work again

--- donkeycar/parts/actuators/actuators.py
+++ donkeycar/parts/actuators/actuators.py
@@ -1,19 +1,17 @@
 """
 actuators.py
-
-Classes to control the motors and servos. These classes
+Classes to control the motors and servos. These classes 
 are wrapped in a mixer class before being used in the drive loop.
-
 """
 
 import time
 
 from ... import utils
 
-
+        
 class PCA9685:
-    '''
-    PWM motor controler using PCA9685 boards.
+    ''' 
+    PWM motor controler using PCA9685 boards. 
     This is used for most RC Cars
     '''
     def __init__(self, channel, frequency=60):
@@ -24,10 +22,122 @@ class PCA9685:
         self.channel = channel
 
     def set_pulse(self, pulse):
-        self.pwm.set_pwm(self.channel, 0, pulse)
+        self.pwm.set_pwm(self.channel, 0, pulse) 
 
     def run(self, pulse):
         self.set_pulse(pulse)
+        
+class PWMSteering:
+    """
+    Wrapper over a PWM motor cotnroller to convert angles to PWM pulses.
+    """
+    LEFT_ANGLE = -1 
+    RIGHT_ANGLE = 1
+
+    def __init__(self, controller=None,
+                       left_pulse=290,
+                       right_pulse=490):
+
+        self.controller = controller
+        self.left_pulse = left_pulse
+        self.right_pulse = right_pulse
+
+
+    def run(self, angle):
+        #map absolute angle to angle that vehicle can implement.
+        pulse = utils.map_range(angle, 
+                                self.LEFT_ANGLE, self.RIGHT_ANGLE,
+                                self.left_pulse, self.right_pulse)
+
+        self.controller.set_pulse(pulse)
+
+    def shutdown(self):
+        self.run(0) #set steering straight
+
+
+
+class PWMThrottle:
+    """
+    Wrapper over a PWM motor cotnroller to convert -1 to 1 throttle
+    values to PWM pulses.
+    """
+    MIN_THROTTLE = -1
+    MAX_THROTTLE =  1
+
+    def __init__(self, controller=None,
+                       max_pulse=300,
+                       min_pulse=490,
+                       zero_pulse=350):
+
+        self.controller = controller
+        self.max_pulse = max_pulse
+        self.min_pulse = min_pulse
+        self.zero_pulse = zero_pulse
+        
+        #send zero pulse to calibrate ESC
+        self.controller.set_pulse(self.zero_pulse)
+        time.sleep(1)
+
+
+    def run(self, throttle):
+        if throttle > 0:
+            pulse = utils.map_range(throttle,
+                                    0, self.MAX_THROTTLE, 
+                                    self.zero_pulse, self.max_pulse)
+        else:
+            pulse = utils.map_range(throttle,
+                                    self.MIN_THROTTLE, 0, 
+                                    self.min_pulse, self.zero_pulse)
+
+        self.controller.set_pulse(pulse)
+        
+    def shutdown(self):
+        self.run(0) #stop vehicle
+
+
+
+class Adafruit_DCMotor_Hat:
+    ''' 
+    Adafruit DC Motor Controller 
+    Used for each motor on a differential drive car.
+    '''
+    def __init__(self, motor_num):
+        from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+        import atexit
+        
+        self.FORWARD = Adafruit_MotorHAT.FORWARD
+        self.BACKWARD = Adafruit_MotorHAT.BACKWARD
+        self.mh = Adafruit_MotorHAT(addr=0x60) 
+        
+        self.motor = self.mh.getMotor(motor_num)
+        self.motor_num = motor_num
+        
+        atexit.register(self.turn_off_motors)
+        self.speed = 0
+        self.throttle = 0
+    
+        
+    def run(self, speed):
+        '''
+        Update the speed of the motor where 1 is full forward and
+        -1 is full backwards.
+        '''
+        if speed > 1 or speed < -1:
+            raise ValueError( "Speed must be between 1(forward) and -1(reverse)")
+        
+        self.speed = speed
+        self.throttle = int(utils.map_range(abs(speed), -1, 1, -255, 255))
+        
+        if speed > 0:            
+            self.motor.run(self.FORWARD)
+        else:
+            self.motor.run(self.BACKWARD)
+            
+        self.motor.setSpeed(self.throttle)
+        
+
+    def shutdown(self):
+        self.mh.getMotor(self.motor_num).run(Adafruit_MotorHAT.RELEASE)
 
 class Maestro:
     '''
@@ -199,123 +309,3 @@ class Teensy:
             ret = ret.rstrip()
 
         return ret
-
-class PWMSteering:
-    """
-    Wrapper over a PWM motor cotnroller to convert angles to PWM pulses.
-    """
-    LEFT_ANGLE = -1
-    RIGHT_ANGLE = 1
-
-    def __init__(self, controller=None,
-                       left_pulse=290,
-                       right_pulse=490):
-
-        self.controller = controller
-        self.left_pulse = left_pulse
-        self.right_pulse = right_pulse
-
-
-    def run(self, angle, mode = None):
-        #map absolute angle to angle that vehicle can implement.
-        pulse = utils.map_range(angle,
-                                self.LEFT_ANGLE, self.RIGHT_ANGLE,
-                                self.left_pulse, self.right_pulse)
-
-        if mode != 'user':
-            self.controller.set_pulse(pulse)
-
-        self.controller.set_turn_left(angle < -0.2)
-        self.controller.set_turn_right(angle > 0.2)
-
-    def shutdown(self):
-        self.run(0) #set steering straight
-
-
-
-class PWMThrottle:
-    """
-    Wrapper over a PWM motor cotnroller to convert -1 to 1 throttle
-    values to PWM pulses.
-    """
-    MIN_THROTTLE = -1
-    MAX_THROTTLE =  1
-
-    def __init__(self, controller=None,
-                       max_pulse=300,
-                       min_pulse=490,
-                       zero_pulse=350):
-
-        self.controller = controller
-        self.max_pulse = max_pulse
-        self.min_pulse = min_pulse
-        self.zero_pulse = zero_pulse
-
-        #send zero pulse to calibrate ESC
-        self.controller.set_pulse(self.zero_pulse)
-        time.sleep(1)
-
-
-    def run(self, throttle, mode = None):
-        if throttle > 0:
-            pulse = utils.map_range(throttle,
-                                    0, self.MAX_THROTTLE,
-                                    self.zero_pulse, self.max_pulse)
-        else:
-            pulse = utils.map_range(throttle,
-                                    self.MIN_THROTTLE, 0,
-                                    self.min_pulse, self.zero_pulse)
-
-        if mode != 'user':
-            self.controller.set_pulse(pulse)
-
-        self.controller.set_brake(throttle < 0)
-        self.controller.set_headlight(throttle < -0.02 or throttle > 0.02)
-
-    def shutdown(self):
-        self.run(0) #stop vehicle
-
-
-
-class Adafruit_DCMotor_Hat:
-    '''
-    Adafruit DC Motor Controller
-    Used for each motor on a differential drive car.
-    '''
-    def __init__(self, motor_num):
-        from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
-        import atexit
-
-        self.FORWARD = Adafruit_MotorHAT.FORWARD
-        self.BACKWARD = Adafruit_MotorHAT.BACKWARD
-        self.mh = Adafruit_MotorHAT(addr=0x60)
-
-        self.motor = self.mh.getMotor(motor_num)
-        self.motor_num = motor_num
-
-        atexit.register(self.turn_off_motors)
-        self.speed = 0
-        self.throttle = 0
-
-
-    def run(self, speed):
-        '''
-        Update the speed of the motor where 1 is full forward and
-        -1 is full backwards.
-        '''
-        if speed > 1 or speed < -1:
-            raise ValueError( "Speed must be between 1(forward) and -1(reverse)")
-
-        self.speed = speed
-        self.throttle = int(utils.map_range(abs(speed), -1, 1, -255, 255))
-
-        if speed > 0:
-            self.motor.run(self.FORWARD)
-        else:
-            self.motor.run(self.BACKWARD)
-
-        self.motor.setSpeed(self.throttle)
-
-
-    def shutdown(self):
-        self.mh.getMotor(self.motor_num).run(Adafruit_MotorHAT.RELEASE)
