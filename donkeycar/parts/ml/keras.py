5ec223cb6792e41a1f1a814769eff1cc7155ef22
Rico <rico@Cristinas-Air.attlocal.net>
2017-08-17 22:21:17 -0700

    Changed 'val_loss' to 'angle_out_loss'.  Errored out when trying to check for 'val_loss'. Found this solution on Slack

--- donkeycar/parts/ml/keras.py
+++ donkeycar/parts/ml/keras.py
@@ -37,14 +37,16 @@ class KerasPilot():
 
         #checkpoint to save model after each epoch
         save_best = keras.callbacks.ModelCheckpoint(saved_model_path, 
-                                                    monitor='val_loss', 
-                                                    verbose=1, 
+                                                    #monitor='val_loss',
+                                                    monitor='angle_out_loss',
+                                                    verbose=1,
                                                     save_best_only=True, 
                                                     mode='min')
         
         #stop training if the validation error stops improving.
-        early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', 
-                                                   min_delta=.0005, 
+        early_stop = keras.callbacks.EarlyStopping(#monitor='val_loss',
+                                                   monitor='angle_out_loss',
+                                                   min_delta=.0005,
                                                    patience=4, 
                                                    verbose=1, 
                                                    mode='auto')
