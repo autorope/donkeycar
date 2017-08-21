6b33ba78c19145cf256cc71f5b6e20a9bc2ff1f1
Will Roscoe <wroscoe@gmail.com>
2017-08-21 14:19:51 -0700

    add ImgCrop part

--- donkeycar/parts/cv/cv.py
+++ donkeycar/parts/cv/cv.py
@@ -1,14 +1,14 @@
 import cv2
 
-class Greyscale():
+class ImgGreyscale():
 
     def run(self, img_arr):
         img_arr = cv2.cvtColor(img_arr, cv2.COLOR_RGB2GRAY)
         return img_arr
-    
-    
-    
-class Canny():
+
+
+
+class ImgCanny():
 
     def __init__(self, low_threshold=60, high_threshold=110):
         self.low_threshold = low_threshold
@@ -22,7 +22,7 @@ class Canny():
 
     
 
-class GaussianBlur():
+class ImgGaussianBlur():
 
     def __init__(self, kernal_size=5):
         self.kernal_size = kernal_size
@@ -30,6 +30,24 @@ class GaussianBlur():
     def run(self, img_arr):
         return cv2.GaussianBlur(img_arr, 
                                 (self.kernel_size, self.kernel_size), 0)
+
+
+
+class ImgCrop:
+    """
+    Crop an image to an area of interest. 
+    """
+    def __init__(self, top=0, bottom=0, left=0, right=0):
+        self.top = top
+        self.bottom = bottom
+        self.left = left
+        self.right = right
+        
+    def run(self, img_arr):
+        width, height, _ = img_arr.shape
+        img_arr = img_arr[self.top:height-self.bottom, 
+                          self.left: width-self.right]
+        return img_arr
         
         
         
