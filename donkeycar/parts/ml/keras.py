751a4701f2dd2b1aec248da46c6e65c1b14545df
Rico <ricorx7@gmail.com>
2017-09-09 16:00:56 -0700

    Update comments

--- donkeycar/parts/ml/keras.py
+++ donkeycar/parts/ml/keras.py
@@ -104,19 +104,19 @@ def default_categorical():
     x = img_in
     x = Convolution2D(24, (5,5), strides=(2,2), activation='relu')(x)       # 24 features, 5 pixel x 5 pixel kernel (convolution, feauture) window, 2wx2h stride, relu activation
     x = Convolution2D(32, (5,5), strides=(2,2), activation='relu')(x)       # 32 features, 5px5p kernel window, 2wx2h stride, relu activatiion
-    x = Convolution2D(64, (5,5), strides=(2,2), activation='relu')(x)       # 64 features
-    x = Convolution2D(64, (3,3), strides=(2,2), activation='relu')(x)       # 64 features
-    x = Convolution2D(64, (3,3), strides=(1,1), activation='relu')(x)       # 64 features
+    x = Convolution2D(64, (5,5), strides=(2,2), activation='relu')(x)       # 64 features, 5px5p kernal window, 2wx2h stride, relu
+    x = Convolution2D(64, (3,3), strides=(2,2), activation='relu')(x)       # 64 features, 3px3p kernal window, 2wx2h stride, relu
+    x = Convolution2D(64, (3,3), strides=(1,1), activation='relu')(x)       # 64 features, 3px3p kernal window, 1wx1h stride, relu
 
     # Possibly add MaxPooling (will make it less sensitive to position in image).  Camera angle fixed, so may not to be needed
 
     x = Flatten(name='flattened')(x)                                        # Flatten to 1D (Fully connected)
-    x = Dense(100, activation='relu')(x)                                    # Reduce the number of neurons to 100, make all negatives 0
+    x = Dense(100, activation='relu')(x)                                    # Classify the data into 100 features, make all negatives 0
     x = Dropout(.1)(x)                                                      # Randomly drop out (turn off) 10% of the neurons (Prevent overfitting)
-    x = Dense(50, activation='relu')(x)                                     # Reduce the number of neurons to 50, make all negatives 0
+    x = Dense(50, activation='relu')(x)                                     # Classify the data into 50 features, make all negatives 0
     x = Dropout(.1)(x)                                                      # Randomly drop out 10% of the neurons (Prevent overfitting)
     #categorical output of the angle
-    angle_out = Dense(15, activation='softmax', name='angle_out')(x)        # Softmax Reduce to 15 categories and find best one based off percentage 0.0-1.0
+    angle_out = Dense(15, activation='softmax', name='angle_out')(x)        # Connect every input with every output and output 15 hidden units. Use Softmax to give percentage. 15 categories and find best one based off percentage 0.0-1.0
     
     #continous output of throttle
     throttle_out = Dense(1, activation='relu', name='throttle_out')(x)      # Reduce to 1 number, Positive number only
