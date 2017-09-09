9c4227d912ed51f0029b44cd9f710ace05a9757d
Rico <ricorx7@gmail.com>
2017-09-09 11:59:49 -0700

    Added comments about neural network

--- donkeycar/parts/ml/keras.py
+++ donkeycar/parts/ml/keras.py
@@ -100,24 +100,26 @@ def default_categorical():
     from keras.layers import Convolution2D, MaxPooling2D, Reshape, BatchNormalization
     from keras.layers import Activation, Dropout, Flatten, Dense
     
-    img_in = Input(shape=(120, 160, 3), name='img_in')                       # Shape comes from camera.py resolution
+    img_in = Input(shape=(120, 160, 3), name='img_in')                      # First layer, input layer, Shape comes from camera.py resolution, RGB
     x = img_in
-    x = Convolution2D(24, (5,5), strides=(2,2), activation='relu')(x)
-    x = Convolution2D(32, (5,5), strides=(2,2), activation='relu')(x)
-    x = Convolution2D(64, (5,5), strides=(2,2), activation='relu')(x)
-    x = Convolution2D(64, (3,3), strides=(2,2), activation='relu')(x)
-    x = Convolution2D(64, (3,3), strides=(1,1), activation='relu')(x)
-    
-    x = Flatten(name='flattened')(x)
-    x = Dense(100, activation='relu')(x)
-    x = Dropout(.1)(x)
-    x = Dense(50, activation='relu')(x)
-    x = Dropout(.1)(x)
+    x = Convolution2D(24, (5,5), strides=(2,2), activation='relu')(x)       # 24 features, 5 pixel x 5 pixel kernel (convolution, feauture) window, 2wx2h stride, relu activation
+    x = Convolution2D(32, (5,5), strides=(2,2), activation='relu')(x)       # 32 features, 5px5p kernel window, 2wx2h stride, relu activatiion
+    x = Convolution2D(64, (5,5), strides=(2,2), activation='relu')(x)       # 64 features
+    x = Convolution2D(64, (3,3), strides=(2,2), activation='relu')(x)       # 64 features
+    x = Convolution2D(64, (3,3), strides=(1,1), activation='relu')(x)       # 64 features
+
+    # Possibly add MaxPooling (will make it less sensitive to position in image).  Camera angle fixed, so may not to be needed
+
+    x = Flatten(name='flattened')(x)                                        # Flatten to 1D (Fully connected)
+    x = Dense(100, activation='relu')(x)                                    # Reduce the number of neurons to 100, make all negatives 0
+    x = Dropout(.1)(x)                                                      # Randomly drop out (turn off) 10% of the neurons (Prevent overfitting)
+    x = Dense(50, activation='relu')(x)                                     # Reduce the number of neurons to 50, make all negatives 0
+    x = Dropout(.1)(x)                                                      # Randomly drop out 10% of the neurons (Prevent overfitting)
     #categorical output of the angle
-    angle_out = Dense(15, activation='softmax', name='angle_out')(x)            # Softmax to find best one between 0-1
+    angle_out = Dense(15, activation='softmax', name='angle_out')(x)        # Softmax Reduce to 15 categories and find best one based off percentage 0.0-1.0
     
     #continous output of throttle
-    throttle_out = Dense(1, activation='relu', name='throttle_out')(x)          # Positive number only
+    throttle_out = Dense(1, activation='relu', name='throttle_out')(x)      # Reduce to 1 number, Positive number only
     
     model = Model(inputs=[img_in], outputs=[angle_out, throttle_out])
     
@@ -125,7 +127,7 @@ def default_categorical():
     model.compile(optimizer='rmsprop',
                   loss={'angle_out': 'categorical_crossentropy', 
                         'throttle_out': 'mean_absolute_error'},
-                  loss_weights={'angle_out': 0.9, 'throttle_out': .1})
+                  loss_weights={'angle_out': 0.9, 'throttle_out': 0.1})
 
     return model
 
@@ -146,7 +148,7 @@ def default_linear():
     x = Convolution2D(64, (3,3), strides=(1,1), activation='relu')(x)
     
     x = Flatten(name='flattened')(x)
-    x = Dense(100, activation='linear')(x)
+    x = Dense(100, activation='linear')(x)                                  # Linear vs ReLU, Linear includes negatives
     x = Dropout(.1)(x)
     x = Dense(50, activation='linear')(x)
     x = Dropout(.1)(x)
