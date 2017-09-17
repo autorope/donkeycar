dd97d6a5fb07426b784fb9ba8edf7fa68368ff40
Will Roscoe <wroscoe@gmail.com>
2017-09-17 14:34:18 -0700

    added config to donkey2_with_joystic

--- donkeycar/templates/config_defaults.py
+++ donkeycar/templates/config_defaults.py
@@ -1,3 +1,18 @@
+""" 
+CAR CONFIG 
+
+This file is read by your car application's manage.py script to change the car
+performance. 
+
+EXMAPLE
+-----------
+import dk
+cfg = dk.load_config(config_path='~/d2/config.py')
+print(cfg.CAMERA_RESOLUTION)
+
+"""
+
+
 import os
 
 #PATHS
@@ -10,7 +25,7 @@ DRIVE_LOOP_HZ = 20
 MAX_LOOPS = 100000
 
 #CAMERA
-CAMERA_RESOLUTION = (120,160)
+CAMERA_RESOLUTION = (160, 120)
 CAMERA_FRAMERATE = DRIVE_LOOP_HZ
 
 #STEERING
@@ -20,6 +35,12 @@ STEERING_RIGHT_PWM = 360
 
 #THROTTLE
 THROTTLE_CHANNEL = 0
-THROTTLE_FORWARD_PWM = 390
+THROTTLE_FORWARD_PWM = 400
 THROTTLE_STOPPED_PWM = 360
-THROTTLE_REVERSE_PWM = 290
+THROTTLE_REVERSE_PWM = 310
+
+
+
+#JOYSTICK
+#JOYSTICK_MAX_THROTTLE=.25
+#JOYSTICK_STEERING_SCALE=1
