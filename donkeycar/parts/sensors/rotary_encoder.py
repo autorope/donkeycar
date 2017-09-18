a284a3c259f186f042a4302b6cd31cc00a090454
Alan Wells <alan@adub.net>
2017-09-17 21:18:41 -0700

    bug fixes for rotary encoder

--- donkeycar/parts/sensors/rotary_encoder.py
+++ donkeycar/parts/sensors/rotary_encoder.py
@@ -5,14 +5,15 @@ Rotary Encoder
 import time
 
 class RotaryEncoder():
-    def __init__(self, m_per_tick=0.0329, pin=23):
+    def __init__(self, mm_per_tick=0.306096, pin=27, poll_delay=0.0166):
         import RPi.GPIO as GPIO
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.add_event_detect(pin, GPIO.BOTH, callback=self.isr)
+        GPIO.setup(pin, GPIO.IN)
+        GPIO.add_event_detect(pin, GPIO.RISING, callback=self.isr)
 
         # initialize the odometer values
-        self.m_per_tick = m_per_tick
+        self.m_per_tick = mm_per_tick / 1000.0
+        self.poll_delay = poll_delay
         self.meters = 0
         self.last_time = time.time()
         self.meters_per_second = 0
@@ -25,25 +26,35 @@ class RotaryEncoder():
     def update(self):
         # keep looping infinitely until the thread is stopped
         while(self.on):
-            if(self.counter > 0):
                 
-                #save the ticks and reset the counter
-                ticks = self.counter
-                self.counter = 0
-                
-                #save off the last time interval and reset the timer
-                start_time = self.last_time
-                end_time = time.time()
-                self.last_time = end_time
-                
-                #calculate elapsed time and distance traveled
-                seconds = end_time - start_time
-                distance = ticks * self.m_per_tick
-                velocity = distance / seconds
-                
-                #update the odometer values
-                self.meters += distance
-                self.meters_per_second = velocity
+            #save the ticks and reset the counter
+            ticks = self.counter
+            self.counter = 0
+            
+            #save off the last time interval and reset the timer
+            start_time = self.last_time
+            end_time = time.time()
+            self.last_time = end_time
+            
+            #calculate elapsed time and distance traveled
+            seconds = end_time - start_time
+            distance = ticks * self.m_per_tick
+            velocity = distance / seconds
+            
+            #update the odometer values
+            self.meters += distance
+            self.meters_per_second = velocity
+
+            #console output for debugging
+            if(ticks > 0):
+                print('seconds:', seconds)
+                print('distance:', distance)
+                print('velocity:', velocity)
+
+                print('distance (m):', round(self.meters, 4))
+                print('velocity (m/s):', self.meters_per_second)
+
+            time.sleep(self.poll_delay)
 
     def run_threaded(self):
         return self.meters, self.meters_per_second
@@ -52,6 +63,7 @@ class RotaryEncoder():
         # indicate that the thread should be stopped
         self.on = False
         print('stopping Rotary Encoder')
+        print('top speed (m/s):', self.top_speed)
         time.sleep(.5)
         
         import RPi.GPIO as GPIO
