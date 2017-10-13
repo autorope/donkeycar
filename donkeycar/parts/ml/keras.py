d65e1c5450d0158b778471850887f943d8c0bc83
Tawn Kramer <tawnkramer@gmail.com>
2017-10-13 11:18:53 -0700

    keep wanting to turn verbose off. added now as argument to train, along with others. default to on, as before.

--- donkeycar/parts/ml/keras.py
+++ donkeycar/parts/ml/keras.py
@@ -28,7 +28,8 @@ class KerasPilot():
     
     
     def train(self, train_gen, val_gen, 
-              saved_model_path, epochs=100, steps=100, train_split=0.8):
+              saved_model_path, epochs=100, steps=100, train_split=0.8,
+              verbose=1, min_delta=.0005, patience=5, use_early_stop=True):
         
         """
         train_gen: generator that yields an array of images an array of 
@@ -38,18 +39,21 @@ class KerasPilot():
         #checkpoint to save model after each epoch
         save_best = keras.callbacks.ModelCheckpoint(saved_model_path, 
                                                     monitor='val_loss', 
-                                                    verbose=0, 
+                                                    verbose=verbose, 
                                                     save_best_only=True, 
                                                     mode='min')
         
         #stop training if the validation error stops improving.
         early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', 
-                                                   min_delta=.0005, 
-                                                   patience=5, 
-                                                   verbose=0, 
+                                                   min_delta=min_delta, 
+                                                   patience=patience, 
+                                                   verbose=verbose, 
                                                    mode='auto')
         
-        callbacks_list = [save_best, early_stop]
+        callbacks_list = [save_best]
+
+        if use_early_stop:
+            callbacks_list.append(early_stop)
         
         hist = self.model.fit_generator(
                         train_gen, 
