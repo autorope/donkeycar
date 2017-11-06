d43a44d2ec174245b0f2dabb57ffa6665fbc380d
Tawn Kramer <tawnkramer@gmail.com>
2017-11-06 11:03:43 -0800

    added poll delay to imu part

--- donkeycar/parts/imu.py
+++ donkeycar/parts/imu.py
@@ -1,3 +1,4 @@
+import time
 
 class Mpu6050:
     '''
@@ -13,17 +14,19 @@ class Mpu6050:
     pip install mpu6050-raspberrypi
     '''
 
-    def __init__(self, addr=0x68):
+    def __init__(self, addr=0x68, poll_delay=0.0166):
         from mpu6050 import mpu6050
         self.sensor = mpu6050(addr)
         self.accel = { 'x' : 0., 'y' : 0., 'z' : 0. }
         self.gyro = { 'x' : 0., 'y' : 0., 'z' : 0. }
         self.temp = 0.
+        self.poll_delay = poll_delay
         self.on = True
 
     def update(self):
         while self.on:
             self.poll()
+            time.sleep(self.poll_delay)
                 
     def poll(self):
         self.accel, self.gyro, self.temp = self.sensor.get_all_data()
@@ -40,7 +43,6 @@ class Mpu6050:
 
 
 if __name__ == "__main__":
-    import time
     iter = 0
     p = Mpu6050()
     while iter < 100:
