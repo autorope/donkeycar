c3908099c8aff7378b90205772bb9aaf96666e0a
Tawn Kramer <tawnkramer@gmail.com>
2017-12-14 08:11:22 -0800

    trip dep on donkeycar

--- scripts/profile.py
+++ scripts/profile.py
@@ -9,9 +9,26 @@ Options:
 """
 import os
 from docopt import docopt
-from donkeycar.parts.simulation import FPSTimer
 import keras
 import numpy as np
+import time
+
+class FPSTimer(object):
+    def __init__(self):
+        self.t = time.time()
+        self.iter = 0
+
+    def reset(self):
+        self.t = time.time()
+        self.iter = 0
+
+    def on_frame(self):
+        self.iter += 1
+        if self.iter == 100:
+            e = time.time()
+            print('fps', 100.0 / (e - self.t))
+            self.t = time.time()
+            self.iter = 0
 
 def profile(model_path):
 
