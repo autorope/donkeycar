a571878d450a1f5040e2c209fafd7a633838c4b5
Tawn Kramer <tawnkramer@gmail.com>
2017-12-14 10:00:31 -0800

    added a button map for specifying joystick controls

--- donkeycar/parts/controller.py
+++ donkeycar/parts/controller.py
@@ -28,7 +28,7 @@ class Joystick():
             0x03 : 'rx',
             0x04 : 'ry',
             0x05 : 'rz',
-            0x06 : 'trottle',
+            0x06 : 'throttle',
             0x07 : 'rudder',
             0x08 : 'wheel',
             0x09 : 'gas',
@@ -51,10 +51,10 @@ class Joystick():
         }
 
         self.button_names = {
-            0x120 : 'trigger',
+            0x120 : 'select',
             0x121 : 'thumb',
             0x122 : 'thumb2',
-            0x123 : 'top',
+            0x123 : 'start',
             0x124 : 'top2',
             0x125 : 'pinkie',
             0x126 : 'base',
@@ -202,11 +202,9 @@ class JoystickController(object):
     ES_THROTTLE_NEG_TWO = 3
 
     def __init__(self, poll_delay=0.0,
-                 max_throttle=1.0,
-                 steering_axis='x',
-                 throttle_axis='rz',
+                 throttle_scale=1.0,
                  steering_scale=1.0,
-                 throttle_scale=-1.0,
+                 throttle_dir=-1.0,
                  dev_fn='/dev/input/js0',
                  auto_record_on_throttle=True):
 
@@ -215,39 +213,23 @@ class JoystickController(object):
         self.mode = 'user'
         self.poll_delay = poll_delay
         self.running = True
-        self.max_throttle = max_throttle
-        self.steering_axis = steering_axis
-        self.throttle_axis = throttle_axis
-        self.steering_scale = steering_scale
         self.throttle_scale = throttle_scale
+        self.steering_scale = steering_scale
+        self.throttle_dir = throttle_dir
         self.recording = False
         self.constant_throttle = False
         self.auto_record_on_throttle = auto_record_on_throttle
         self.dev_fn = dev_fn
         self.js = None
         self.tub = None
+        self.num_records_to_erase = 100
         self.estop_state = self.ES_IDLE
-        #We expect that the framework for parts will start a new
-        #thread for our update fn. We used to do that and it caused
-        #two threads to be polling for js events.
-
-    def set_tub(self, tub):
-        self.tub = tub
-
-    def erase_last_N_records(self):
-        if self.tub is not None:
-            try:
-                self.tub.erase_last_n_records(100)
-                print('erased last 100 records.')                
-            except:
-                print('failed to erase')
         
-    def on_throttle_changes(self):
-        '''
-        turn on recording when non zero throttle in the user mode.
-        '''
-        if self.auto_record_on_throttle:
-            self.recording = (self.throttle != 0.0 and self.mode == 'user')
+        self.button_down_trigger_map = {}
+        self.button_up_trigger_map = {}
+        self.axis_trigger_map = {}
+
+        self.init_trigger_maps()
 
     def init_js(self):
         '''
@@ -261,7 +243,78 @@ class JoystickController(object):
             self.js = None
         return self.js is not None
 
+    def init_trigger_maps(self):
+        '''
+        init default set of mapping from buttons to function calls
+        '''
+
+        self.button_down_trigger_map = {
+            'select' : self.toggle_mode,
+            'circle' : self.toggle_manual_recording,
+            'triangle' : self.erase_last_N_records,
+            'cross' : self.emergency_stop,
+            'dpad_up' : self.increase_max_throttle,
+            'dpad_down' : self.decrease_max_throttle,
+            'start' : self.toggle_constant_throttle,
+        }
+
+        self.axis_trigger_map = {
+            'x' : self.set_steering,
+            'rz' : self.set_throttle,
+        }
+
+    def print_controls(self):
+        '''
+        print the mapping of buttons and axis to functions
+        '''
+        print("Joystick Controls:")
+        print("On Button Down:")
+        print(self.button_down_trigger_map)
+        print("On Button Up:")
+        print(self.button_up_trigger_map)
+        print("On Axis Move:")
+        print(self.axis_trigger_map)
+
+    def set_button_down_trigger(self, button, func):
+        '''
+        assign a string button descriptor to a given function call
+        '''
+        self.button_down_trigger_map[button] = func
+
+    def set_button_up_trigger(self, button, func):
+        '''
+        assign a string button descriptor to a given function call
+        '''
+        self.button_up_trigger_map[button] = func
+
+    def set_axis_trigger(self, axis, func):
+        '''
+        assign a string axis descriptor to a given function call
+        '''
+        self.axis_trigger_map[axis] = func
+
+    def set_tub(self, tub):
+        self.tub = tub
+
+    def erase_last_N_records(self):
+        if self.tub is not None:
+            try:
+                self.tub.erase_last_n_records(self.num_records_to_erase)
+                print('erased last %d records.' % self.num_records_to_erase)                
+            except:
+                print('failed to erase')
+        
+    def on_throttle_changes(self):
+        '''
+        turn on recording when non zero throttle in the user mode.
+        '''
+        if self.auto_record_on_throttle:
+            self.recording = (self.throttle != 0.0 and self.mode == 'user')
+
     def emergency_stop(self):
+        '''
+        initiate a series of steps to try to stop the vehicle as quickly as possible
+        '''
         print('E-Stop!!!')
         self.mode = "user"
         self.recording = False
@@ -269,27 +322,9 @@ class JoystickController(object):
         self.estop_state = self.ES_START
         self.throttle = 0.0
 
-
     def update(self):
         '''
         poll a joystick for input events
-
-        button map name => PS3 button => function
-        * top2 = PS3 dpad up => increase throttle scale
-        * base = PS3 dpad down => decrease throttle scale
-        * base2 = PS3 dpad left => increase steering scale 
-        * pinkie = PS3 dpad right => decrease steering scale
-        * trigger = PS3 select => switch modes
-        * top = PS3 start => toggle constant throttle
-        * base5 = PS3 left trigger 1 
-        * base3 = PS3 left trigger 2
-        * base6 = PS3 right trigger 1 
-        * base4 = PS3 right trigger 2
-        * thumb2 = PS3 right thumb
-        * thumb = PS3 left thumb
-        * circle = PS3 circrle => toggle recording
-        * triangle = PS3 triangle => increase max throttle
-        * cross = PS3 cross => decrease max throttle
         '''
 
         #wait for joystick to be online
@@ -298,98 +333,107 @@ class JoystickController(object):
 
         while self.running:
             button, button_state, axis, axis_val = self.js.poll()
-        
-            if axis == self.steering_axis:
-                self.angle = self.steering_scale * axis_val
-                print("angle", self.angle)
 
-            if axis == self.throttle_axis:
-                #this value is often reversed, with positive value when pulling down
-                self.throttle = (self.throttle_scale * axis_val * self.max_throttle)
-                print("throttle", self.throttle)
-                self.on_throttle_changes()
-            
-            if button == 'trigger' and button_state == 1:
+            if axis and axis_val and axis in self.axis_trigger_map:
                 '''
-                switch modes from:
-                user: human controlled steer and throttle
-                local_angle: ai steering, human throttle
-                local: ai steering, ai throttle
+                then invoke the function attached to that axis
                 '''
-                if self.mode == 'user':
-                    self.mode = 'local_angle'
-                elif self.mode == 'local_angle':
-                    self.mode = 'local'
-                else:
-                    self.mode = 'user'
-                print('new mode:', self.mode)
+                self.axis_trigger_map[axis](axis_val)
 
-            if button == 'circle' and button_state == 1:
+            if button and button_state == 1 and button in self.button_down_trigger_map:
                 '''
-                toggle recording on/off
+                then invoke the function attached to that button
                 '''
-                if self.auto_record_on_throttle:
-                    print('auto record on throttle is enabled.')
-                elif self.recording:
-                    self.recording = False
-                else:
-                    self.recording = True
+                self.button_down_trigger_map[button]()
 
-                print('recording:', self.recording)
-
-            if button == 'triangle' and button_state == 1:
+            if button and button_state == 0 and button in self.button_up_trigger_map:
                 '''
-                erase last N records
+                then invoke the function attached to that button
                 '''
-                self.erase_last_N_records()
-
-            if button == 'cross' and button_state == 1:
-                '''
-                erase last N records
-                '''
-                self.emergency_stop()
-
-            if button == 'dpad_up' and button_state == 1:
-                '''
-                increase max throttle setting
-                '''
-                self.max_throttle = round(min(1.0, self.max_throttle + 0.01), 2)
-                if self.constant_throttle:
-                    self.throttle = self.max_throttle
-                    self.on_throttle_changes()
-
-                print('max_throttle:', self.max_throttle)
-
-            if button == 'dpad_down' and button_state == 1:
-                '''
-                decrease max throttle setting
-                '''
-                self.max_throttle = round(max(0.0, self.max_throttle - 0.01), 2)
-                if self.constant_throttle:
-                    self.throttle = self.max_throttle
-                    self.on_throttle_changes()
-                    
-                print('max_throttle:', self.max_throttle)
-
-            if button == 'top' and button_state == 1:
-                '''
-                toggle constant throttle
-                '''
-                if self.constant_throttle:
-                    self.constant_throttle = False
-                    self.throttle = 0
-                    self.on_throttle_changes()
-                else:
-                    self.constant_throttle = True
-                    self.throttle = self.max_throttle
-                    self.on_throttle_changes()
-                print('constant_throttle:', self.constant_throttle)           
+                self.button_up_trigger_map[button]()
 
             time.sleep(self.poll_delay)
+
+    def set_steering(self, axis_val):
+        self.angle = self.steering_scale * axis_val
+        print("angle", self.angle)
+
+    def set_throttle(self, axis_val):
+        #this value is often reversed, with positive value when pulling down
+        self.throttle = (self.throttle_dir * axis_val * self.throttle_scale)
+        print("throttle", self.throttle)
+        self.on_throttle_changes()
+
+    def toggle_manual_recording(self):
+        '''
+        toggle recording on/off
+        '''
+        if self.auto_record_on_throttle:
+            print('auto record on throttle is enabled.')
+        elif self.recording:
+            self.recording = False
+        else:
+            self.recording = True
+
+        print('recording:', self.recording)
+
+    def increase_max_throttle(self):
+        '''
+        increase throttle scale setting
+        '''
+        self.throttle_scale = round(min(1.0, self.throttle_scale + 0.01), 2)
+        if self.constant_throttle:
+            self.throttle = self.throttle_scale
+            self.on_throttle_changes()
+
+        print('throttle_scale:', self.throttle_scale)
+
+    def decrease_max_throttle(self):
+        '''
+        decrease throttle scale setting
+        '''
+        self.throttle_scale = round(max(0.0, self.throttle_scale - 0.01), 2)
+        if self.constant_throttle:
+            self.throttle = self.throttle_scale
+            self.on_throttle_changes()
+
+        print('throttle_scale:', self.throttle_scale)
+
+    def toggle_constant_throttle(self):
+        '''
+        toggle constant throttle
+        '''
+        if self.constant_throttle:
+            self.constant_throttle = False
+            self.throttle = 0
+            self.on_throttle_changes()
+        else:
+            self.constant_throttle = True
+            self.throttle = self.throttle_scale
+            self.on_throttle_changes()
+        print('constant_throttle:', self.constant_throttle)           
+
+    def toggle_mode(self):
+        '''
+        switch modes from:
+        user: human controlled steer and throttle
+        local_angle: ai steering, human throttle
+        local: ai steering, ai throttle
+        '''
+        if self.mode == 'user':
+            self.mode = 'local_angle'
+        elif self.mode == 'local_angle':
+            self.mode = 'local'
+        else:
+            self.mode = 'user'
+        print('new mode:', self.mode)
 
     def run_threaded(self, img_arr=None):
         self.img_arr = img_arr
 
+        '''
+        process E-Stop state machine
+        '''
         if self.estop_state > self.ES_IDLE:
             if self.estop_state == self.ES_START:
                 self.estop_state = self.ES_THROTTLE_NEG_ONE
@@ -402,7 +446,7 @@ class JoystickController(object):
                 self.throttle = -1.0
                 return 0.0, self.throttle, self.mode, False
             elif self.estop_state == self.ES_THROTTLE_NEG_TWO:
-                self.throttle += 0.1
+                self.throttle += 0.05
                 if self.throttle >= 0.0:
                     self.estop_state = self.ES_IDLE
                 return 0.0, self.throttle, self.mode, False
