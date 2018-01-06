2c52c2eb6381a1caa2850e083d69ba0542fcf275
Tawn Kramer <tawnkramer@gmail.com>
2018-01-06 10:29:58 -0800

    add back two wheel

--- donkeycar/parts/actuator.py
+++ donkeycar/parts/actuator.py
@@ -330,3 +330,80 @@ class MockController(object):
 
     def shutdown(self):
         pass
+
+
+class L298N_HBridge_DC_Motor(object):
+    '''
+    Motor controlled with an L298N hbridge from the gpio pins on Rpi
+    '''
+    def __init__(self, pin_forward, pin_backward, pwm_pin):
+        import RPi.GPIO as GPIO
+        self.pin_forward = pin_forward
+        self.pin_backward = pin_backward
+        self.pwm_pin = pwm_pin
+
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.pin_forward, GPIO.OUT)
+        GPIO.setup(self.pin_backward, GPIO.OUT)
+        GPIO.setup(self.pwm_pin, GPIO.OUT)
+
+        freq = 50 #HZ
+        self.pwm = GPIO.PWM(self.pwm_pin, freq)
+        self.pwm.start(0)
+
+    def run(self, speed):
+        import RPi.GPIO as GPIO
+        '''
+        Update the speed of the motor where 1 is full forward and
+        -1 is full backwards.
+        '''
+        if speed > 1 or speed < -1:
+            raise ValueError( "Speed must be between 1(forward) and -1(reverse)")
+        
+        self.speed = speed
+        max_duty = 90 #I've read 90 is a good max
+        self.throttle = int(utils.map_range(speed, -1, 1, -max_duty, max_duty))
+        
+        if self.throttle > 0:
+            self.pwm.ChangeDutyCycle(self.throttle)
+            GPIO.output(self.pin_forward, GPIO.HIGH)
+            GPIO.output(self.pin_backward, GPIO.LOW)
+        elif self.throttle < 0:
+            self.pwm.ChangeDutyCycle(-self.throttle)
+            GPIO.output(self.pin_forward, GPIO.LOW)
+            GPIO.output(self.pin_backward, GPIO.HIGH)
+        else:
+            self.pwm.ChangeDutyCycle(self.throttle)
+            GPIO.output(self.pin_forward, GPIO.LOW)
+            GPIO.output(self.pin_backward, GPIO.LOW)
+
+
+    def shutdown(self):
+        import RPi.GPIO as GPIO
+        self.pwm.stop()
+        GPIO.cleanup()
+
+
+class TwoWheelSteeringThrottle(object):
+
+    def run(self, throttle, steering):
+        if throttle > 1 or throttle < -1:
+            raise ValueError( "throttle must be between 1(forward) and -1(reverse)")
+ 
+        if steering > 1 or steering < -1:
+            raise ValueError( "steering must be between 1(right) and -1(left)")
+
+        left_motor_speed = throttle
+        right_motor_speed = throttle
+ 
+        if steering < 0:
+            left_motor_speed *= (1.0 - (-steering))
+        elif steering > 0:
+            right_motor_speed *= (1.0 - steering)
+
+        return left_motor_speed, right_motor_speed
+
+    def shutdown(self):
+        pass
+
+
