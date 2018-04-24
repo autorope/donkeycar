b0e92f4f9f16758b68e85858c0c028d799fbe548
Will Roscoe <wroscoe@gmail.com>
2018-04-23 22:17:24 -0700

    updates to install script

--- install/make_pi_disk_img.sh
+++ install/make_pi_disk_img.sh
@@ -14,8 +14,8 @@ sudo apt install xsel xclip -y
 sudo apt install python3-h5py
 
 #remove python2 (1 min)
-sudo apt-get remove python2.7
-sudo apt-get autoremove
+sudo apt-get remove python2.7 -y
+sudo apt-get autoremove -y
 
 
 #create a python virtualenv (2 min)
@@ -27,28 +27,30 @@ source ~/.bashrc
 
 #install numpy and pandas (3 min)
 sudo apt install libxml2-dev python3-lxml -y
-sudo apt install libxslt-dev
-pip install pandas
+sudo apt install libxslt-dev -y
+
+# install numpy which is needed for opencv install
+pip install pandas #also installs numpy
 
 
 #install opencv (1 hour)
 #instructions from:https://raspberrypi.stackexchange.com/questions/69169/how-to-install-opencv-on-raspberry-pi-3-in-raspbian-jessie
-sudo apt-get install build-essential git cmake pkg-config
-sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-sudo apt-get install libxvidcore-dev libx264-dev
-sudo apt-get install libatlas-base-dev gfortran
+sudo apt-get install build-essential git cmake pkg-config -y
+sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev -y
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y
+sudo apt-get install libxvidcore-dev libx264-dev -y
+sudo apt-get install libatlas-base-dev gfortran -y
 
-git clone https://github.com/opencv/opencv.git
-git clone https://github.com/opencv/opencv_contrib.git
-pip install numpy
+git clone https://github.com/opencv/opencv.git --depth 1
+git clone https://github.com/opencv/opencv_contrib.git --depth 1
+
 cd ~/opencv
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
     -D CMAKE_INSTALL_PREFIX=/usr/local \
     -D INSTALL_C_EXAMPLES=OFF \
-    -D INSTALL_PYTHON_EXAMPLES=ON \
+    -D INSTALL_PYTHON_EXAMPLES=OFF \
     -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
     -D BUILD_EXAMPLES=OFF ..
 make -j4
