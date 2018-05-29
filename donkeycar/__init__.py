b096528c071af69b0e477f136416b583382fdb4a
Will Roscoe <wroscoe@gmail.com>
2018-05-28 19:46:23 -0700

    fix manifest to include all html js and css files

--- donkeycar/__init__.py
+++ donkeycar/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '2.2.3'
+__version__ = '2.2.4'
 
 print('using donkey v{} ...'.format(__version__))
 
--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 import os
 
-#include the non python files
+
+# include the non python files
 def package_files(directory, strip_leading):
     paths = []
     for (path, directories, filenames) in os.walk(directory):
@@ -13,65 +14,65 @@ def package_files(directory, strip_leading):
 
 
 setup(name='donkeycar',
-    version='2.2.3',
-    description='Self driving library for python.',
-    url='https://github.com/wroscoe/donkey',
-    download_url='https://github.com/wroscoe/donkey/archive/2.1.5.tar.gz',
-    author='Will Roscoe',
-    author_email='wroscoe@gmail.com',
-    license='MIT',
-    entry_points={
-        'console_scripts': [
-            'donkey=donkeycar.management.base:execute_from_command_line',
-        ],
-    },
-    install_requires=['numpy', 
-                      'pillow',
-                      'docopt',
-                      'tornado==4.5.3',
-                      'requests',
-                      'keras==2.0.8',
-                      'h5py',
-                      'python-socketio',
-                      'flask',
-                      'eventlet',
-                      'moviepy',
-                      'pandas',
-                      'tensorflow>=1.1'
-                     ],
-
-    extras_require={
-                    'pi': [
-                        'picamera',
-                        'Adafruit_PCA9685',
+      version='2.2.4',
+      description='Self driving library for python.',
+      url='https://github.com/wroscoe/donkey',
+      download_url='https://github.com/wroscoe/donkey/archive/2.1.5.tar.gz',
+      author='Will Roscoe',
+      author_email='wroscoe@gmail.com',
+      license='MIT',
+      entry_points={
+          'console_scripts': [
+              'donkey=donkeycar.management.base:execute_from_command_line',
+          ],
+      },
+      install_requires=['numpy',
+                        'pillow',
+                        'docopt',
+                        'tornado==4.5.3',
+                        'requests',
+                        'keras==2.0.8',
+                        'h5py',
+                        'python-socketio',
+                        'flask',
+                        'eventlet',
+                        'moviepy',
+                        'pandas',
+                        'tensorflow>=1.1'
                         ],
-                    'dev': ['pytest']
-                    },
 
-    include_package_data=True,
+      extras_require={
+          'pi': [
+              'picamera',
+              'Adafruit_PCA9685',
+          ],
+          'dev': ['pytest']
+      },
 
-    classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+      include_package_data=True,
 
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+      classifiers=[
+          # How mature is this project? Common values are
+          #   3 - Alpha
+          #   4 - Beta
+          #   5 - Production/Stable
+          'Development Status :: 3 - Alpha',
 
-        # Pick your license as you wish (should match "license" above)
-         'License :: OSI Approved :: MIT License',
+          # Indicate who your project is intended for
+          'Intended Audience :: Developers',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence',
 
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
+          # Pick your license as you wish (should match "license" above)
+          'License :: OSI Approved :: MIT License',
 
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-    keywords='selfdriving cars drive',
+          # Specify the Python versions you support here. In particular, ensure
+          # that you indicate whether you support Python 2, Python 3 or both.
 
-    packages=find_packages(exclude=(['tests', 'docs', 'site', 'env'])),
-)
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+      ],
+      keywords='selfdriving cars donkeycar diyrobocars',
+
+      packages=find_packages(exclude=(['tests', 'docs', 'site', 'env'])),
+      )
