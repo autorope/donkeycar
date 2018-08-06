6fb0559b231807530d9e0a051e17e389df154841
Bartosz Kosiorek <gang65@poczta.onet.pl>
2018-08-06 10:16:29 +0000

    Increase ISO level to 1600, to reduce blurring caused by fast move. With this fix, the NN is learning much more effective.

--- donkeycar/parts/camera.py
+++ donkeycar/parts/camera.py
@@ -20,6 +20,7 @@ class PiCamera(BaseCamera):
         self.camera = PiCamera()  # PiCamera gets resolution (height, width)
         self.camera.resolution = resolution
         self.camera.framerate = framerate
+        self.camera.iso = 1600
         self.rawCapture = PiRGBArray(self.camera, size=resolution)
         self.stream = self.camera.capture_continuous(self.rawCapture,
                                                      format="rgb",
