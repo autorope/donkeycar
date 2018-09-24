b5626c144835e4d105969b337f5bcae34f9d5226
Tawn Kramer <tawnkramer@gmail.com>
2018-09-24 09:58:46 -0700

    fix some keras2 api warnings

--- donkeycar/parts/keras.py
+++ donkeycar/parts/keras.py
@@ -603,7 +603,7 @@ def build_3d_cnn(w, h, d, s, num_outputs):
     # Second layer
     model.add(Conv3D(
         filters=16, kernel_size=(3,3,3), strides=(1,3,3),
-        data_format='channels_last', border_mode='same')
+        data_format='channels_last', padding='same')
     )
     model.add(Activation('relu'))
     model.add(MaxPooling3D(
@@ -612,7 +612,7 @@ def build_3d_cnn(w, h, d, s, num_outputs):
     # Third layer
     model.add(Conv3D(
         filters=32, kernel_size=(3,3,3), strides=(1,1,1),
-        data_format='channels_last', border_mode='same')
+        data_format='channels_last', padding='same')
     )
     model.add(Activation('relu'))
     model.add(MaxPooling3D(
@@ -621,7 +621,7 @@ def build_3d_cnn(w, h, d, s, num_outputs):
     # Fourth layer
     model.add(Conv3D(
         filters=64, kernel_size=(3,3,3), strides=(1,1,1),
-        data_format='channels_last', border_mode='same')
+        data_format='channels_last', padding='same')
     )
     model.add(Activation('relu'))
     model.add(MaxPooling3D(
@@ -630,7 +630,7 @@ def build_3d_cnn(w, h, d, s, num_outputs):
     # Fifth layer
     model.add(Conv3D(
         filters=128, kernel_size=(3,3,3), strides=(1,1,1),
-        data_format='channels_last', border_mode='same')
+        data_format='channels_last', padding='same')
     )
     model.add(Activation('relu'))
     model.add(MaxPooling3D(
