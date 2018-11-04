4e1297b833dfa155846ec87e82eb3dc8e6e72185
Will Roscoe <wroscoe@gmail.com>
2018-11-04 15:38:09 -0800

    remove 3.7 python from testing because travisci complains

--- setup.py
+++ setup.py
@@ -70,7 +70,6 @@ setup(name='donkeycar',
 
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7',
       ],
       keywords='selfdriving cars donkeycar diyrobocars',
 
