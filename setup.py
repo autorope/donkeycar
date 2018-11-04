7f695d1b996fd43b102d1c69bf814ebcf4049bc3
Will Roscoe <wroscoe@gmail.com>
2018-11-04 15:09:39 -0800

    stop testing 3.4

--- setup.py
+++ setup.py
@@ -68,9 +68,9 @@ setup(name='donkeycar',
           # Specify the Python versions you support here. In particular, ensure
           # that you indicate whether you support Python 2, Python 3 or both.
 
-          'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
       ],
       keywords='selfdriving cars donkeycar diyrobocars',
 
