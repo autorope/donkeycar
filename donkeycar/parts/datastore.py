c903c8b778f54c60b3d37a3c024cde9fd0c7fe1e
SergeM <sbmatyunin@gmail.com>
2019-03-26 06:02:00 +0100

    BUGFIX fixed get_record_gen when shuffle==False

--- donkeycar/parts/datastore.py
+++ donkeycar/parts/datastore.py
@@ -290,9 +290,11 @@ class Tub(object):
             df = self.get_df()
 
         while True:
-            for _ in self.df.iterrows():
+            for _, row in self.df.iterrows():
                 if shuffle:
                     record_dict = df.sample(n=1).to_dict(orient='record')[0]
+                else:
+                    record_dict = row
 
                 record_dict = self.read_record(record_dict)
 
