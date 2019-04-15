41b9395113d99e45afc57fd09162be2f4fe1ef78
Tawn Kramer <tawnkramer@gmail.com>
2019-04-14 18:48:55 -0700

    added realsense part for t265

---
+++ donkeycar/parts/realsense.py
@@ -0,0 +1,58 @@
+'''
+Author: Tawn Kramer
+File: realsense.py
+Date: April 14 2019
+Notes: Parts to input data from Intel Realsense cameras
+'''
+import time
+import pyrealsense2 as rs
+
+class RS_T265(object):
+    '''
+    The Intel Realsense T265 camera is a device which uses an imu, twin fisheye cameras,
+    and an Movidius chip to do sensor fusion and emit a world space coordinate frame that 
+    is remarkably consistent.
+    '''
+
+    def __init__(self):
+        # Declare RealSense pipeline, encapsulating the actual device and sensors
+        self.pipe = rs.pipeline()
+        cfg = rs.config()
+        cfg.enable_stream(rs.stream.pose)
+
+        # Start streaming with requested config
+        self.pipe.start(cfg)
+        self.running = True
+        
+        zero_vec = (0.0, 0.0, 0.0)
+        self.pos = zero_vec
+        self.vel = zero_vec
+        self.acc = zero_vec
+
+    def poll(self):
+        frames = self.pipe.wait_for_frames()
+
+        # Fetch pose frame
+        pose = frames.get_pose_frame()
+
+        if pose:
+            data = pose.get_pose_data()
+            self.pos = data.translation
+            self.vel = data.velocity
+            self.acc = data.accleration
+
+    def update(self):
+        while self.running:
+            self.poll()
+
+    def run_threaded(self):
+        return self.pos, self.vel, self.acc
+
+    def run(self):
+        self.poll()
+        return self.run_threaded()
+
+    def shutdown(self):
+        self.running = False
+        time.sleep(0.1)
+        self.pipe.stop()
