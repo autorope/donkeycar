e41e6f382aef24429e3511f5ae6f32b94b66f398
jay-johnson <jay.p.h.johnson@gmail.com>
2019-06-03 00:13:58 -0700

    fixes for resizing the 2nd partition with e2fsck + resize2fs. rc.local sh fixes for if statement syntax

--- install/pi/extend-sd-card.sh
+++ install/pi/extend-sd-card.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 set -e
 
+# stack overflow on this:
+# https://serverfault.com/questions/870594/resize-partition-to-maximum-using-parted-in-non-interactive-mode
+#
+# blog used to help make this:
+# https://geekpeek.net/resize-filesystem-fdisk-resize2fs/
+
+if [[ "${DCPATH}" == "" ]]; then
+    export DCPATH="."
+fi
+if [[ -e ${DCPATH}/files/bash_colors.sh ]]; then
+    source ${DCPATH}/files/bash_colors.sh
+fi
+
 if [[ $# -eq 0 ]] ; then
     echo 'please tell me the device to resize as the first parameter, like /dev/sda'
     exit 1
 fi
-
 
 if [[ $# -eq 1 ]] ; then
     echo 'please tell me the partition number to resize as the second parameter, like 1 in case you mean /dev/sda1 or 4, if you mean /dev/sda2'
@@ -28,9 +40,38 @@ MAXSIZEMB=`printf %s\\n 'unit MB print list' | parted | grep "Disk ${DEVICE}" | 
 echo "[ok] would/will resize to from ${CURRENTSIZE}MB to ${MAXSIZEMB}MB "
 
 if [[ "$APPLY" == "apply" ]] ; then
-  echo "[ok] applying resize operation.."
-  parted -s ${DEVICE} resizepart ${PARTNR} ${MAXSIZEMB}
-  echo "[done]"
+    anmt "ok applying resize operation.."
+    anmt "parted -s ${DEVICE} resizepart ${PARTNR} ${MAXSIZEMB}"
+    parted -s ${DEVICE} resizepart ${PARTNR} ${MAXSIZEMB}
+    if [[ "$?" != "0" ]]; then
+        err "failed to resize partition with: parted -s ${DEVICE} resizepart ${PARTNR} ${MAXSIZEMB}"
+        exit 1
+    else
+        anmt "done - parted"
+    fi
+    anmt "fixing partitions:"
+    anmt "e2fsck -y -f ${DEVICE}2"
+    e2fsck -y -f ${DEVICE}2
+    if [[ "$?" != "0" ]]; then
+        err "failed to e2fsck ${DEVICE}2 with: e2fsck -y -f ${DEVICE}2"
+        exit 1
+    else
+        anmt "done - e2fsck"
+    fi
+    # this should resize the partition to max out the storage space
+    anmt "resize2fs ${DEVICE}2"
+    resize2fs ${DEVICE}2
+    if [[ "$?" != "0" ]]; then
+        err "failed to resize2fs ${DEVICE}2 with: resize2fs ${DEVICE}2"
+        exit 1
+    else
+        anmt "done - resize"
+    fi
 else
-  echo "[WARNING]!: Sandbox mode, i did not size!. Use 'apply' as the 3d parameter to apply the changes"
+    err "[WARNING]!: Sandbox mode, did not detect size correctly!. Use 'apply' as the 3rd parameter to apply the changes"
+    exit 1
 fi
+
+good "done - extend-sd-card.sh"
+
+exit 0
