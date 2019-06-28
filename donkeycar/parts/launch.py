850d1f1cda30e976a55bd293030d61f610c3ca5d
Tawn Kramer <tawnkramer@gmail.com>
2019-06-28 15:53:39 -0700

    add launcher test

--- donkeycar/parts/launch.py
+++ donkeycar/parts/launch.py
@@ -7,14 +7,14 @@ class AiLaunch():
     up to speed.
     '''
 
-    def __init__(self, launch_duration=1.0, launch_throttle=1.0, keep_enabled=False):
+    def __init__(self, launch_duration=1.0, launch_throttle=1.0, trigger_on_switch=False):
         self.active = False
-        self.enabled = keep_enabled
+        self.enabled = False
         self.timer_start = None
         self.timer_duration = launch_duration
         self.launch_throttle = launch_throttle
         self.prev_mode = None
-        self.keep_enabled = keep_enabled
+        self.trigger_on_switch = trigger_on_switch
         
     def do_enable(self):
         self.enabled = True
@@ -25,21 +25,25 @@ class AiLaunch():
 
         if mode != self.prev_mode:
             self.prev_mode = mode
+            if mode == "local" and self.trigger_on_switch:
+                self.enabled = True
 
-            if mode == "local" and self.enabled:
-                if not self.active:
-                    self.active = True
-                    self.timer_start = time.time()
-                else:
-                    duration = time.time() - self.timer_start
-                    if duration > self.timer_duration:
-                        self.active = False
-                        if not self.keep_enabled:
-                            self.enabled = False
+        if mode == "local" and self.enabled:
+            if not self.active:
+                self.active = True
+                self.timer_start = time.time()
+            else:
+                duration = time.time() - self.timer_start
+                if duration > self.timer_duration:
+                    self.active = False
+                    self.enabled = False
+                    print("duration exceeded.")
+        else:
+            self.active = False
 
-                if self.active:
-                    print('AiLauncher is active!!!')
-                    new_throttle = self.launch_throttle
+        if self.active:
+            print('AiLauncher is active!!!')
+            new_throttle = self.launch_throttle
 
         return new_throttle
 
