b7849460588a6e365192cc9d0073625c9812681f
Tawn Kramer <tawnkramer@gmail.com>
2019-07-11 15:22:02 -0700

    change profile to load uff or tflite models

--- scripts/profile.py
+++ scripts/profile.py
@@ -2,14 +2,14 @@
 Script to drive a TF model as fast as possible
 
 Usage:
-    profile.py (--model=<model>)
+    profile.py (--model=<model>) (--type=<linear|categorical|etc>)
     
 Options:
     -h --help        Show this screen.
 """
 import os
 from docopt import docopt
-from tensorflow.python import keras
+import donkeycar as dk
 import numpy as np
 import time
 
@@ -30,17 +30,17 @@ class FPSTimer(object):
             self.t = time.time()
             self.iter = 0
 
-def profile(model_path):
+def profile(model_path, model_type):
+    cfg = dk.load_config('config.py')
+    model_path = os.path.expanduser(model_path)
+    model = dk.utils.get_model_by_type(model_type, cfg)
+    model.load(model_path)
+    
+    count, h, w, ch = 1, cfg.TARGET_H, cfg.TARGET_W, cfg.TARGET_D
+    seq_len = 0
 
-    #load keras model
-    model = keras.models.load_model(model_path)
-
-    #query the input to see what it likes
-    try:
-        count, h, w, ch = model.inputs[0].get_shape()
-        seq_len = 0
-    except:
-        count, seq_len, h, w, ch = model.inputs[0].get_shape()
+    if "rnn" in model_type or "3d" in model_type:
+        seq_len = cfg.SEQUENCE_LENGTH
 
     #generate random array in the right shape
     img = np.random.rand(int(h), int(w), int(ch))
@@ -61,9 +61,9 @@ def profile(model_path):
             run forward pass on model
             '''
             if seq_len:
-                model.predict(img_arr[None, :, :, :, :])
+                model.run(img_arr)
             else:
-                model.predict(img[None, :, :, :])
+                model.run(img)
 
             '''
             keep track of iterations and give feed back on iter/sec
@@ -76,4 +76,4 @@ def profile(model_path):
 
 if __name__ == '__main__':
     args = docopt(__doc__)
-    profile(model_path = args['--model'])
+    profile(model_path = args['--model'], model_type = args['--type'])
