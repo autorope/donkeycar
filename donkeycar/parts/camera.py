ee4674f71c81373b98f7ace9e5f6578fc45fe476
Tawn Kramer <tawnkramer@gmail.com>
2019-08-19 23:55:55 -0700

    improve CSIC camera

--- donkeycar/parts/camera.py
+++ donkeycar/parts/camera.py
@@ -128,34 +128,41 @@ class CSICamera(BaseCamera):
     '''
     Camera for Jetson Nano IMX219 based camera
     Credit: https://github.com/feicccccccc/donkeycar/blob/dev/donkeycar/parts/camera.py
+    gstreamer init string from https://github.com/NVIDIA-AI-IOT/jetbot/blob/master/jetbot/camera.py
     '''
-    def gstreamer_pipeline(self,capture_width=120, capture_height=160, display_width=120, display_height=160, framerate=20, flip_method=0) :   
-        return ('nvarguscamerasrc ! ' 
-        'video/x-raw(memory:NVMM), '
-        'width=(int)%d, height=(int)%d, '
-        'format=(string)NV12, framerate=(fraction)%d/1 ! '
-        'nvvidconv flip-method=%d ! '
-        'video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! '
-        'videoconvert ! '
-        'video/x-raw, format=(string)BGR ! appsink'  % (capture_width,capture_height,framerate,flip_method,display_width,display_height))
+    def gstreamer_pipeline(self, capture_width=3280, capture_height=2464, output_width=224, output_height=224, framerate=21, flip_method=0) :   
+        return 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=%d, height=%d, format=(string)NV12, framerate=(fraction)%d/1 ! nvvidconv flip-method=%d ! nvvidconv ! video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! videoconvert ! appsink' % (
+                capture_width, capture_height, framerate, flip_method, output_width, output_height)
     
-    def __init__(self, image_w=160, image_h=120, image_d=3, framerate=60, gstreamer_flip=0):
+    def __init__(self, image_w=160, image_h=120, image_d=3, capture_width=3280, capture_height=2464, framerate=60, gstreamer_flip=0):
+        '''
+        gstreamer_flip = 0 - no flip
+        gstreamer_flip = 1 - rotate CCW 90
+        gstreamer_flip = 2 - flip vertically
+        gstreamer_flip = 3 - rotate CW 90
+        '''
         self.w = image_w
         self.h = image_h
         self.running = True
         self.frame = None
         self.flip_method = gstreamer_flip
+        self.capture_width = capture_width
+        self.capture_height = capture_height
+        self.framerate = framerate
 
     def init_camera(self):
         import cv2
 
         # initialize the camera and stream
-        self.camera = cv2.VideoCapture(\
-            self.gstreamer_pipeline(\
-                display_width=self.w,\
-                    display_height=self.h,\
-                    flip_method=self.flip_method),
-                    cv2.CAP_GSTREAMER)
+        self.camera = cv2.VideoCapture(
+            self.gstreamer_pipeline(
+                capture_width =self.capture_width,
+                capture_height =self.capture_height,
+                output_width=self.w,
+                output_height=self.h,
+                framerate=self.framerate,
+                flip_method=self.flip_method),
+            cv2.CAP_GSTREAMER)
 
         self.poll_camera()
         print('CSICamera loaded.. .warming camera')
