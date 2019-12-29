d8815780d881cac2c49d41046b6c30920730f8b2
Michał Sochoń <kaszpir@gmail.com>
2019-12-29 20:14:35 +0100

    Run black formatter on setup.py

--- setup.py
+++ setup.py
@@ -2,103 +2,89 @@ from setuptools import setup, find_packages
 
 import os
 
-#include the non python files
+# include the non python files
 def package_files(directory, strip_leading):
     paths = []
     for (path, directories, filenames) in os.walk(directory):
         for filename in filenames:
             package_file = os.path.join(path, filename)
-            paths.append(package_file[len(strip_leading):])
+            paths.append(package_file[len(strip_leading) :])
     return paths
 
-car_templates=['templates/*']
-web_controller_html = package_files('donkeycar/parts/controllers/templates', 'donkeycar/')
+
+car_templates = ["templates/*"]
+web_controller_html = package_files(
+    "donkeycar/parts/controllers/templates", "donkeycar/"
+)
 
 
 extra_files = car_templates + web_controller_html
-print('extra_files', extra_files)
+print("extra_files", extra_files)
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
-setup(name='donkeycar',
-    version='3.1.1',
-    long_description = long_description,
-    description='Self driving library for python.',
-    url='https://github.com/autorope/donkeycar',
-    author='Will Roscoe, Adam Conway, Tawn Kramer',
-    author_email='wroscoe@gmail.com, adam@casaconway.com, tawnkramer@gmail.com',
-    license='MIT',
+setup(
+    name="donkeycar",
+    version="3.1.1",
+    long_description=long_description,
+    description="Self driving library for python.",
+    url="https://github.com/autorope/donkeycar",
+    author="Will Roscoe, Adam Conway, Tawn Kramer",
+    author_email="wroscoe@gmail.com, adam@casaconway.com, tawnkramer@gmail.com",
+    license="MIT",
     entry_points={
-        'console_scripts': [
-            'donkey=donkeycar.management.base:execute_from_command_line',
+        "console_scripts": [
+            "donkey=donkeycar.management.base:execute_from_command_line",
         ],
     },
-    install_requires=['numpy', 
-                      'pillow',
-                      'docopt',
-                      'tornado',
-                      'requests',
-                      'h5py',
-                      'moviepy',
-                      'pandas',
-                      'PrettyTable',
-                      'paho-mqtt'
-                     ],
-
+    install_requires=[
+        "numpy",
+        "pillow",
+        "docopt",
+        "tornado",
+        "requests",
+        "h5py",
+        "moviepy",
+        "pandas",
+        "PrettyTable",
+        "paho-mqtt",
+    ],
     extras_require={
-                    'pi': [
-                        'picamera',
-                        'Adafruit_PCA9685',
-                        'Adafruit_SSD1306',
-                        'RPi.GPIO',
-                        'pyserial',
-                        ],
-                    'nano': [
-                        'Adafruit_PCA9685',
-                        'Adafruit_SSD1306',
-                        ],
-                    'pc': [
-                        'matplotlib',
-                        ],
-                    'dev' : [
-                        'pytest',
-                        'pytest-cov',
-                        'responses',
-                        ],
-                    'ci': ['codecov'],
-                    'tf': ['tensorflow>=1.9.0'],
-                    'tf_gpu': ['tensorflow-gpu>=1.9.0'],
-                    },
-    package_data={
-        'donkeycar': extra_files, 
-        },
-
-      include_package_data=True,
-
-      classifiers=[
-          # How mature is this project? Common values are
-          #   3 - Alpha
-          #   4 - Beta
-          #   5 - Production/Stable
-          'Development Status :: 3 - Alpha',
-
-          # Indicate who your project is intended for
-          'Intended Audience :: Developers',
-          'Topic :: Scientific/Engineering :: Artificial Intelligence',
-
-          # Pick your license as you wish (should match "license" above)
-          'License :: OSI Approved :: MIT License',
-
-          # Specify the Python versions you support here. In particular, ensure
-          # that you indicate whether you support Python 2, Python 3 or both.
-
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7',
-      ],
-      keywords='selfdriving cars donkeycar diyrobocars',
-
-      packages=find_packages(exclude=(['tests', 'docs', 'site', 'env'])),
-      )
+        "pi": [
+            "picamera",
+            "Adafruit_PCA9685",
+            "Adafruit_SSD1306",
+            "RPi.GPIO",
+            "pyserial",
+        ],
+        "nano": ["Adafruit_PCA9685", "Adafruit_SSD1306",],
+        "pc": ["matplotlib",],
+        "dev": ["pytest", "pytest-cov", "responses", 'black; python_version>="3.6"',],
+        "ci": ["codecov"],
+        "tf": ["tensorflow>=1.9.0"],
+        "tf_gpu": ["tensorflow-gpu>=1.9.0"],
+    },
+    package_data={"donkeycar": extra_files,},
+    include_package_data=True,
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 3 - Alpha",
+        # Indicate who your project is intended for
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        # Pick your license as you wish (should match "license" above)
+        "License :: OSI Approved :: MIT License",
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords="selfdriving cars donkeycar diyrobocars",
+    packages=find_packages(exclude=(["tests", "docs", "site", "env"])),
+)
