ffdd2f30a33f2ae59d62999563c2493fa10b0aff
DocGarbanzo <dirk.prange@web.de>
2020-09-23 22:34:17 +0100

    * Remove obsolete commented code and unused arguments + small pep-8 formatting improvements

--- donkeycar/config.py
+++ donkeycar/config.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Wed Sep 13 21:27:44 2017
@@ -7,11 +6,11 @@ Created on Wed Sep 13 21:27:44 2017
 """
 import os
 import types
-    
+
+
 class Config:
     
-    def from_pyfile(self, filename, silent=False):
-        #filename = os.path.join(self.root_path, filename)
+    def from_pyfile(self, filename):
         d = types.ModuleType('config')
         d.__file__ = filename
         try:
@@ -26,7 +25,6 @@ class Config:
     def from_object(self, obj):
         for key in dir(obj):
             if key.isupper():
-                #self[key] = getattr(obj, key)
                 setattr(self, key, getattr(obj, key))
                 
     def __str__(self):
