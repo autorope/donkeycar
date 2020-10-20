b770ec433d39ed392ec852a61c4cefbb7ab2f10c
wallarug <wallarug@coliemore.com.au>
2020-10-20 22:24:53 +1100

    added in brake functionality for simulator only
    fix typo in drivemode return which checked pilot/throttle instead of brake

--- donkeycar/parts/dgym.py
+++ donkeycar/parts/dgym.py
@@ -24,7 +24,7 @@ class DonkeyGymEnv(object):
         conf['guid'] = 0
         self.env = gym.make(env_name, conf=conf)
         self.frame = self.env.reset()
-        self.action = [0.0, 0.0]
+        self.action = [0.0, 0.0, 0.0]
         self.running = True
         self.info = { 'pos' : (0., 0., 0.)}
         self.delay = float(delay)
@@ -33,13 +33,15 @@ class DonkeyGymEnv(object):
         while self.running:
             self.frame, _, _, self.info = self.env.step(self.action)
 
-    def run_threaded(self, steering, throttle):
+    def run_threaded(self, steering, throttle, brake=None):
         if steering is None or throttle is None:
             steering = 0.0
             throttle = 0.0
+        if brake is None:
+            brake = 0.0
         if self.delay > 0.0:
             time.sleep(self.delay / 1000.0)
-        self.action = [steering, throttle]
+        self.action = [steering, throttle, brake]
         return self.frame
 
     def shutdown(self):
