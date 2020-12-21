c5caae2cd1afe8faac2880385aaa6ff35badf677
DocGarbanzo <dirk.prange@web.de>
2020-12-21 22:51:10 +0000

    Tub improvements: * Fixed manifest iterator to avoid too deep recursive function calls which segfaults when deleting many records * Automatic addition of '_session_id' field in the Tub. This mimics the naming convention we used to have for old-style tubs before: YY-MM-DD_N, where N is a running integer. All session ids of a tub are stored in the manifest.json file. The tub needs to receive records and needs to get closed for this entry to be updated. * Added test for the new tub and tub writer logic
    Tub GUI development:
    * Support starting from 'donkey ui' command
    * All relevant settings are stored in ~/.donkeyrc file which is editable
    
    Training improvements:
    * You can now put a predicate for tub records into the myconfig.py to filter records for training. The config variable TRAIN_FILTER has to be set to that function.
    * Improve training performance by using tf prefetch
    * Default AI framework to 'tensorflow' in training
    
    Others:
    * Update docs
    * Update patch version number to 4.2.0
    * Reduced run time of test_train using only 5 epochs and adjusting thresholds accordingly
    * Reduced run time of torch tests using 3 epochs - these were very slow (like 25+ m on MacBook)
    * Minor updates in docstrings and switching to f-strings
    * Fixed duplicated function in keras.py

--- donkeycar/pipeline/types.py
+++ donkeycar/pipeline/types.py
@@ -4,7 +4,7 @@ from typing import Any, List, Optional, TypeVar, Tuple
 import numpy as np
 from donkeycar.config import Config
 from donkeycar.parts.tub_v2 import Tub
-from donkeycar.utils import load_image, load_pil_image, normalize_image, train_test_split
+from donkeycar.utils import load_image, load_pil_image, train_test_split
 from typing_extensions import TypedDict
 
 X = TypeVar('X', covariant=True)
@@ -60,14 +60,15 @@ class TubRecord(object):
         else:
             _image = self._image
         return _image
+
     def __repr__(self) -> str:
         return repr(self.underlying)
 
 
 class TubDataset(object):
-    '''
+    """
     Loads the dataset, and creates a train/test split.
-    '''
+    """
 
     def __init__(self, config: Config, tub_paths: List[str],
                  shuffle: bool = True) -> None:
@@ -77,15 +78,20 @@ class TubDataset(object):
         self.tubs: List[Tub] = [Tub(tub_path, read_only=True)
                                 for tub_path in self.tub_paths]
         self.records: List[TubRecord] = list()
+        self.train_filter = getattr(config, 'TRAIN_FILTER', None)
 
     def train_test_split(self) -> Tuple[List[TubRecord], List[TubRecord]]:
-        print(f'Loading tubs from paths {self.tub_paths}')
+        msg = f'Loading tubs from paths {self.tub_paths}' + f' with filter ' \
+              f'{self.train_filter}' if self.train_filter else ''
+        print(msg)
         self.records.clear()
         for tub in self.tubs:
             for underlying in tub:
-                record = TubRecord(self.config, tub.base_path,
-                                   underlying=underlying)
-                self.records.append(record)
+                record = TubRecord(self.config, tub.base_path, underlying)
+                if not self.train_filter or self.train_filter(record):
+                    self.records.append(record)
 
         return train_test_split(self.records, shuffle=self.shuffle,
                                 test_size=(1. - self.config.TRAIN_TEST_SPLIT))
+
+
