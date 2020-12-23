35d6ac8b84ce57113398059b9eeb95de1a415d27
DocGarbanzo <dirk.prange@web.de>
2020-12-23 09:26:55 +0000

    Incorporate PR feedback * Add empty records concept to tub * Minor updates to conversion script

--- scripts/convert_to_tub_v2.py
+++ scripts/convert_to_tub_v2.py
@@ -35,28 +35,32 @@ def convert_to_tub_v2(paths, output_path):
 
         record_paths = legacy_tub.gather_records()
         bar = IncrementalBar('Converting', max=len(record_paths))
-        prev_rec_num = None
+        previous_index = None
         for record_path in record_paths:
             try:
                 contents = Path(record_path).read_text()
                 record = json.loads(contents)
                 image_path = record['cam/image_array']
-                rec_num = int(image_path.split('_')[0])
+                current_index = int(image_path.split('_')[0])
                 image_path = os.path.join(legacy_tub.path, image_path)
                 image_data = Image.open(image_path)
                 record['cam/image_array'] = image_data
                 # first record or they are continuous, just append
-                if not prev_rec_num or rec_num == prev_rec_num + 1:
+                if not previous_index or current_index == previous_index + 1:
                     output_tub.write_record(record)
-                    prev_rec_num = rec_num
+                    previous_index = current_index
                 # otherwise fill the gap with dummy records
                 else:
-                    prev_rec_num += 1
-                    while prev_rec_num < rec_num:
+                    # Skipping over previous record here because it has
+                    # already been written.
+                    previous_index += 1
+                    # Adding empty record nodes, and marking them deleted
+                    # until the next valid record.
+                    while previous_index < current_index:
                         idx = output_tub.manifest.current_index
-                        output_tub.write_record({})
+                        output_tub.write_record()
                         output_tub.delete_record(idx)
-                        prev_rec_num += 1
+                        previous_index += 1
                 bar.next()
             except Exception as exception:
                 print(f'Ignoring record path {record_path}\n', exception)
