8e821266013978ba585fc7e39e2cc3696ac3c9dd
Chris Anderson <gca2@earthlink.net>
2021-01-05 11:58:47 -0800

    Fixed missing line in config template

--- donkeycar/templates/cfg_path_follow.py
+++ donkeycar/templates/cfg_path_follow.py
@@ -73,6 +73,7 @@ USE_NETWORKED_JS = False            #should we listen for remote joystick contro
 NETWORK_JS_SERVER_IP = "192.168.0.1"#when listening for network joystick control, which ip is serving this information
 JOYSTICK_DEADZONE = 0.0             # when non zero, this is the smallest throttle before recording triggered.
 JOYSTICK_THROTTLE_DIR = -1.0        # use -1.0 to flip forward/backward, use 1.0 to use joystick's natural forward/backward
+JOYSTICK_DEVICE_FILE = "/dev/input/js0" # this is the unix file use to access the joystick.
 
 
 #SOMBRERO
