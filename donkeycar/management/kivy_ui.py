9f6b942d888e0cbdeba2653ec7687b0c1c69b3b8
DocGarbanzo <dirk.prange@web.de>
2021-01-17 22:26:13 +0000

    First half-working version of tub manager.

--- donkeycar/management/kivy_ui.py
+++ donkeycar/management/kivy_ui.py
@@ -24,6 +24,8 @@ from PIL import Image as PilImage
 
 from donkeycar import load_config
 from donkeycar.management.tub_gui import RcFileHandler
+from donkeycar.parts.tub_v2 import Tub
+from donkeycar.pipeline.types import TubRecord
 
 
 Builder.load_file('ui.kv')
@@ -65,16 +67,73 @@ class ConfigManager(BoxLayout, FileChooserBase):
             try:
                 self.config = load_config(os.path.join(self.file_path, 'config.py'))
                 rc_handler.data['car_dir'] = self.file_path
-                return True
             except FileNotFoundError:
                 print(f'Directory {self.file_path} has no config.py')
             except Exception as e:
                 print(e)
+
+
+class TubManager(BoxLayout, FileChooserBase):
+    """ Class to manage loading or reloading of the Tub from the tub directory.
+        Loading triggers many actions on other widgets of the app. """
+    file_path = rc_handler.data.get('last_tub')
+    tub = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.records = None
+        self.len = 1
+
+    def load_action(self):
+        if self.update_tub(reload=False):
+            rc_handler.data['last_tub'] = self.file_path
+
+    def update_tub(self, reload=False):
+        if not self.file_path:
+            return False
+        if not os.path.exists(os.path.join(self.file_path, 'manifest.json')):
+            self.parent.parent.status(f'Path {self.file_path} is not a valid '
+                                      f'tub.')
+            return False
+        try:
+            self.tub = Tub(self.file_path)
+        except Exception as e:
+            self.parent.parent.status(f'Failed loading tub: {str(e)}')
             return False
 
+        cfg = self.parent.parent.ids.config_manager.config
+        expression = self.parent.parent.ids.tub_filter.filter_expression
 
-class TubManager(BoxLayout):
-    pass
+        # Use filter, this defines the function
+        def select(underlying):
+            if expression is None:
+                return True
+            else:
+                record = TubRecord(cfg, self.tub.base_path, underlying)
+                res = eval(expression)
+                return res
+
+        self.records = [TubRecord(cfg, self.tub.base_path, record)
+                        for record in self.tub if select(record)]
+        self.len = len(self.records)
+        if self.len > 0:
+            # self.state.record = self.records[self.state.i]
+            # # update app components, manipulator, slider and plot
+            # self.app.tub_manipulator.set_lr(is_l=True)
+            # self.app.tub_manipulator.set_lr(is_l=False)
+            # # clear bars for new tub only but not for reloading existing tub
+            # if not reload:
+            #     self.app.data_panel.clear()
+            # self.app.slider.slider.configure(to=self.len - 1)
+            # # update graph
+            # self.app.data_plot.update_dataframe_from_tub()
+            msg = f'Loaded tub {self.file_path} with {self.len} records'
+        else:
+            msg = f'No records in tub {self.file_path}'
+        if self.parent.parent.ids.tub_filter.record_filter:
+            msg += f' using filter {self.parent.parent.ids.tub_filter.record_filter}'
+        self.parent.parent.status(msg)
+        return True
 
 
 class LabelBar(BoxLayout):
@@ -131,6 +190,8 @@ class TubEditor(BoxLayout):
 
 
 class TubFilter(BoxLayout):
+    filter_expression = StringProperty(None)
+    record_filter = StringProperty(None)
     pass
 
 
@@ -139,6 +200,9 @@ class UiLayout(BoxLayout):
 
     def on_index(self, obj, val):
         self.ids.img.update(val)
+
+    def status(self, msg):
+        self.ids.status.text = msg
 
 
 class TubApp(App):
