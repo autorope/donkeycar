d18159e0aa6db5f69f397451ee73ab37889f976c
DocGarbanzo <dirk.prange@web.de>
2021-03-15 08:12:52 +0000

    Revert unintended changes to code from merge from master.

--- donkeycar/parts/controller.py
+++ donkeycar/parts/controller.py
@@ -356,11 +356,11 @@ class PS4Joystick(Joystick):
         self.axis_names = {
             0x00 : 'left_stick_horz',
             0x01 : 'left_stick_vert',
-            0x03 : 'right_stick_horz',
-            0x04 : 'right_stick_vert',
+            0x02 : 'right_stick_horz',
+            0x05 : 'right_stick_vert',
 
-            0x02 : 'left_trigger_axis',
-            0x05 : 'right_trigger_axis',
+            0x03 : 'left_trigger_axis',
+            0x04 : 'right_trigger_axis',
 
             0x10 : 'dpad_leftright',
             0x11 : 'dpad_updown',
@@ -376,21 +376,21 @@ class PS4Joystick(Joystick):
 
         self.button_names = {
 
-            0x134 : 'square',
-            0x130 : 'cross',
-            0x131 : 'circle',
+            0x130 : 'square',
+            0x131 : 'cross',
+            0x132 : 'circle',
             0x133 : 'triangle',
 
-            0x138 : 'L1',
-            0x139 : 'R1',
+            0x134 : 'L1',
+            0x135 : 'R1',
             0x136 : 'L2',
             0x137 : 'R2',
             0x13a : 'L3',
             0x13b : 'R3',
 
             0x13d : 'pad',
-            0x13a : 'share',
-            0x13b : 'options',
+            0x138 : 'share',
+            0x139 : 'options',
             0x13c : 'PS',
         }
 
