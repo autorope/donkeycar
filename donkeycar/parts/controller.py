01cda2e6016909f7904c76ce8af5837f9ceda9ca
Chris Anderson <analoguedevices@gmail.com>
2021-03-16 00:57:55 -0700

    Fixed XboxOne keycodes (#814)
    * Fixed XboxOne keycodes

--- donkeycar/parts/controller.py
+++ donkeycar/parts/controller.py
@@ -517,9 +517,14 @@ class XboxOneJoystick(Joystick):
         super(XboxOneJoystick, self).__init__(*args, **kwargs)
 
         self.axis_names = {
-            0x00: 'left_stick_horz',
-            0x02: 'left_trigger',
-            0x05: 'right_trigger'
+            0x00 : 'left_stick_horz',
+            0x01 : 'left_stick_vert',
+            0x05 : 'right_stick_vert',
+            0x02 : 'right_stick_horz',
+            0x0a : 'left_trigger',
+            0x09 : 'right_trigger',
+            0x10 : 'dpad_horiz',
+            0x11 : 'dpad_vert'
         }
 
         self.button_names = {
@@ -527,11 +532,10 @@ class XboxOneJoystick(Joystick):
             0x131: 'b_button',
             0x133: 'x_button',
             0x134: 'y_button',
+            0x13b: 'options',
             0x136: 'left_shoulder',
             0x137: 'right_shoulder',
-            0x13b: 'options',
         }
-
 
 class LogitechJoystick(Joystick):
     '''
