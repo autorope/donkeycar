ec86e2e5d03d410f8159e0b54ae634fe0b09435f
Chris Anderson <gca2@earthlink.net>
2021-04-08 04:50:50 +0100

    Added support for encoders
    (cherry picked from commit 7b6dfc1a86baf5aad8458d53675b613200dc464b)

--- donkeycar/templates/complete.py
+++ donkeycar/templates/complete.py
@@ -73,6 +73,18 @@ def drive(cfg, model_path=None, use_joystick=False, model_type=None,
         from donkeycar.parts.telemetry import MqttTelemetry
         tel = MqttTelemetry(cfg)
 
+    if cfg.HAVE_ODOM:
+        if cfg.ENCODER_TYPE == "GPIO":
+            from donkeycar.parts.encoder import RotaryEncoder
+            enc = RotaryEncoder(mm_per_tick=0.306096, pin = cfg.ODOM_PIN, debug = cfg.ODOM_DEBUG)
+            V.add(enc, inputs=['throttle'], outputs=['enc/speed'], threaded=True)
+        elif cfg.ENCODER_TYPE == "arduino":
+            from donkeycar.parts.encoder import ArduinoEncoder
+            enc = ArduinoEncoder()
+            V.add(enc, outputs=['enc/speed'], threaded=True)
+        else:
+            print("No supported encoder found")
+
     logger.info("cfg.CAMERA_TYPE %s"%cfg.CAMERA_TYPE)
     if camera_type == "stereo":
 
@@ -349,6 +361,12 @@ def drive(cfg, model_path=None, use_joystick=False, model_type=None,
 
         inputs = ['cam/image_array', "behavior/one_hot_state_array"]
     #IMU
+    elif cfg.USE_LIDAR:
+        inputs = ['cam/image_array', 'lidar/dist_array']
+
+    elif cfg.HAVE_ODOM:
+        inputs = ['cam/image_array', 'enc/speed']
+
     elif model_type == "imu":
         assert(cfg.HAVE_IMU)
         #Run the pilot if the mode is not user.
@@ -557,7 +575,7 @@ def drive(cfg, model_path=None, use_joystick=False, model_type=None,
 
         V.add(left_motor, inputs=['left_motor_speed'])
         V.add(right_motor, inputs=['right_motor_speed'])
-        
+
 
     elif cfg.DRIVE_TRAIN_TYPE == "SERVO_HBRIDGE_PWM":
         from donkeycar.parts.actuator import ServoBlaster, PWMSteering
@@ -610,6 +628,14 @@ def drive(cfg, model_path=None, use_joystick=False, model_type=None,
     else:
         inputs=['cam/image_array','user/angle', 'user/throttle', 'user/mode']
         types=['image_array','float', 'float','str']
+
+    if cfg.USE_LIDAR:
+        inputs += ['lidar/dist_array']
+        types += ['nparray']
+
+    if cfg.HAVE_ODOM:
+        inputs += ['enc/speed']
+        types += ['float']
 
     if cfg.TRAIN_BEHAVIORS:
         inputs += ['behavior/state', 'behavior/label', "behavior/one_hot_state_array"]
@@ -694,4 +720,3 @@ if __name__ == '__main__':
               meta=args['--meta'])
     elif args['train']:
         print('Use python train.py instead.\n')
-
