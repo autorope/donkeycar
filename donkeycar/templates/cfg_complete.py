470f0d6a7f34e34afc9c0d9289e85a817d346a87
JoeSiu <2000.joesiu@gmail.com>
2021-07-13 16:04:20 +0800

    Updated stop sign detector part

--- donkeycar/templates/cfg_complete.py
+++ donkeycar/templates/cfg_complete.py
@@ -373,3 +373,5 @@ REALSENSE_D435_ID = None        # serial number of camera or None if you only ha
 STOP_SIGN_DETECTOR = False
 STOP_SIGN_MIN_SCORE = 0.2
 STOP_SIGN_SHOW_BOUNDING_BOX = True
+STOP_SIGN_MAX_REVERSE_COUNT = 10    # How many times should the car reverse when detected a stop sign, set to 0 to disable reversing
+STOP_SIGN_REVERSE_THROTTLE = -0.5     # Throttle during reversing when detected a stop sign
