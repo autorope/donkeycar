1f8445357c73501e2a886b9c820aa8868bcc75a4
Ezward <emurmur@yahoo.com>
2021-10-17 14:10:04 -0700

    added __main__ test program to pins.py - also made some fixes that I found when testing - currently it appears InputPinGpio and OutputPinGpio   function correctly, but PwmPinGpio does not

--- donkeycar/parts/pins.py
+++ donkeycar/parts/pins.py
@@ -182,7 +182,7 @@ class PwmPin(ABC):
         return PinState.NOT_STARTED  # subclasses must override
 
     @abstractmethod
-    def duty_cycle(self, state:float) -> None:
+    def duty_cycle(self, duty:float) -> None:
         """
         Set the output duty cycle of the pin 
         in range 0 to 1.0 (0% to 100%)
@@ -229,7 +229,7 @@ def output_pin_by_id(pin_id:str, frequency_hz:int=60) -> OutputPin:
         return output_pin(pin_provider, pin_number, pin_scheme=pin_scheme)
 
 
-def pwm_pin_by_id(pin_id:str, frequency_hz:int=60) -> OutputPin:
+def pwm_pin_by_id(pin_id:str, frequency_hz:int=60) -> PwmPin:
     """
     Select a pwm output pin given a pin id.
     """
@@ -249,7 +249,7 @@ def pwm_pin_by_id(pin_id:str, frequency_hz:int=60) -> OutputPin:
         return pwm_pin(pin_provider, pin_number, pin_scheme=pin_scheme, frequency_hz=frequency_hz)
 
 
-def input_pin_by_id(pin_id:str, pull:int=PinPull.PULL_NONE) -> OutputPin:
+def input_pin_by_id(pin_id:str, pull:int=PinPull.PULL_NONE) -> InputPin:
     """
     Select a ttl input pin given a pin id.
     """
@@ -315,8 +315,8 @@ def gpio_fn(pin_scheme, fn):
 
 
 # lookups to convert abstact api to GPIO values
-gpio_pin_edge = [GPIO.RISING, GPIO.FALLING, GPIO.BOTH]
-gpio_pin_pull = [GPIO.PUD_OFF, GPIO.PUD_DOWN, GPIO.PUD_UP]
+gpio_pin_edge = [None, GPIO.RISING, GPIO.FALLING, GPIO.BOTH]
+gpio_pin_pull = [None, GPIO.PUD_OFF, GPIO.PUD_DOWN, GPIO.PUD_UP]
 gpio_pin_scheme = {PinScheme.BOARD: GPIO.BOARD, PinScheme.BCM: GPIO.BCM}
 
 
@@ -340,9 +340,9 @@ class InputPinGpio(InputPin):
         """
         if self.state() != PinState.NOT_STARTED:
             raise RuntimeError("Attempt to start InputPin that is already started.")
-        gpio_fn(self.pin_scheme, lambda: GPIO.setup(self.pin_number, GPIO.IN, pull_up_down=gpio_pin_pull(self.pull)))
+        gpio_fn(self.pin_scheme, lambda: GPIO.setup(self.pin_number, GPIO.IN, pull_up_down=gpio_pin_pull[self.pull]))
         if on_input is not None:
-            gpio_fn(self.pin_scheme, lambda: GPIO.add_event_detect(self.pin_number, gpio_pin_edge(edge), callback=on_input))
+            gpio_fn(self.pin_scheme, lambda: GPIO.add_event_detect(self.pin_number, gpio_pin_edge[edge], callback=on_input))
         self.input()  # read first state
 
     def stop(self) -> None:
@@ -420,7 +420,7 @@ class PwmPinGpio(PwmPin):
         if duty < 0 or duty > 1:
             raise ValueError("duty_cycle must be in range 0 to 1")
         self.pwm.ChangeDutyCycle(duty * 100)
-        self._state = duty_cycle
+        self._state = duty
 
 
 #
@@ -508,3 +508,140 @@ class PwmPinPCA9685(PwmPin):
         self.pca9685.set_duty_cycle(duty)
         self._state = duty
 
+
+if __name__ == '__main__':
+    import argparse
+    from threading import Thread
+    import sys
+    import time
+
+    #
+    # output 50% duty cycle on Rpi board pin 33 (equivalent to BCM.13) for 10 seconds
+    # python pins.py --pwm-pin=RPI_GPIO.BOARD.33 --duty=0.5 --time=10
+    #
+    # input on Rpi board pin 35 (equivalend to BCM.19) for 10 seconds
+    # python pins.py --in-pin=RPI_GPIO.BOARD.35 --time=10
+    #
+    # output 50% duty cycle on Rpi board pin 33, input on Rpi board pin 35 using interrupt handler
+    # python pins.py --pwm-pin=RPI_GPIO.BOARD.33 --duty=0.5 --in-pin=RPI_GPIO.BOARD.35 -int=rising --time=10
+    #
+    # output on Rpi board pin 33, input on Rpi board pin 35 using interrupt handler
+    # python pins.py --out-pin=RPI_GPIO.BOARD.33 --duty=0.5 --in-pin=RPI_GPIO.BOARD.35 -int=rising --time=10
+    #
+    #     
+    # parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--pwm-pin", type=str, default=None,
+                        help="pwm pin id, like 'PCA9685:1:60.13' or 'RPI_GPIO.BCM.13")
+    parser.add_argument("-hz", "--hertz", type=int, default=60, 
+                        help="PWM signal frequence in hertz.  Default is 60hz")
+    parser.add_argument("-d", "--duty", type=float, default=0.5,
+                        help = "duty cycle in range 0 to 1.  Default is 0.5")
+
+    parser.add_argument("-o", "--out-pin", type=str, default=None,
+                        help="ttl output pin id, like 'PCA9685:1:60.13' or 'RPI_GPIO.BOARD.35' or 'RPI_GPIO.BCM.13'")
+
+    parser.add_argument("-i", "--in-pin", type=str, default=None,
+                        help="ttl input pin id, like 'RPI_GPIO.BOARD.35' or 'RPI_GPIO.BCM.19'")
+    parser.add_argument("-pu", "--pull", type=str, choices=['up', 'down', 'none'], default='none',
+                        help="input pin pullup, one of 'up', 'down', 'none'")
+    parser.add_argument("-int", "--interrupt", type=str, choices=['falling', 'rising', 'both', 'none'], default='none',
+                        help = "use interrupt routine on in-pin with given edge; 'falling', 'rising' or 'both'")
+
+
+    parser.add_argument("-tm", "--time", type=float, default=1, help="duration test in seconds")
+    parser.add_argument("-db", "--debug", action='store_true', help = "show debug output")
+    parser.add_argument("-th", "--threaded", action='store_true', help = "run in threaded mode")
+
+    # Read arguments from command line
+    args = parser.parse_args()
+    
+    help = []
+    if args.hertz < 1:
+        help.append("-hz/--hertz: must be >= 1.")
+
+    if args.duty < 0 or args.duty > 1:
+        help.append("-d/--duty: must be in range 0 to 1")
+
+    if args.pwm_pin is None and args.out_pin is None and args.in_pin is None:
+        help.append("must have one of -o/--out-pin or -p/--pwm-pin or -i/--in-pin")
+
+    if args.pwm_pin is not None and args.out_pin is not None:
+        help.append("must have only one of -o/--out-pin or -p/--pwn-pin")
+
+    if args.time < 1:
+        help.append("-tm/--time: must be > 0.")
+                        
+    if len(help) > 0:
+        parser.print_help()
+        for h in help:
+            print("  " + h)
+        sys.exit(1)
+
+    pin_pull = {
+        'up': PinPull.PULL_UP,
+        'down': PinPull.PULL_DOWN,
+        'none': PinPull.PULL_NONE
+    }
+    pin_edge = {
+        'none': None,
+        'falling': PinEdge.FALLING,
+        'rising': PinEdge.RISING,
+        'both': PinEdge.BOTH
+    }
+
+    def on_input(state):
+        if ttl_in_pin.input():
+            print("+") 
+        else:
+            print("-")
+            
+    pwm_out_pin:PwmPin = None
+    ttl_out_pin:OutputPin = None
+    ttl_in_pin:InputPin = None
+    try:
+        #
+        # construct a pin of the correct type
+        #
+        if args.in_pin is not None:
+            ttl_in_pin = input_pin_by_id(args.in_pin, pin_pull[args.pull])
+            if args.interrupt != 'none':
+                ttl_in_pin.start(on_input=on_input, edge=pin_edge[args.interrupt])
+            else:
+                ttl_in_pin.start()
+
+        if args.pwm_pin is not None:
+            pwm_out_pin = pwm_pin_by_id(args.pwm_pin, args.hertz)
+            pwm_out_pin.start(args.duty)
+
+        if args.out_pin is not None:
+            ttl_out_pin = output_pin_by_id(args.out_pin, args.hertz)
+            ttl_out_pin.start(PinState.LOW)
+        
+        start_time = time.time()
+        end_time = start_time + args.time
+        while start_time < end_time:
+            if ttl_out_pin is not None:
+                ttl_out_pin.output(PinState.HIGH)
+                time.sleep(1 / args.hertz * args.duty)
+                ttl_out_pin.output(PinState.LOW)
+                time.sleep(1 / args.hertz * (1 - args.duty))
+            else:
+                # yield time to background threads
+                sleep_time = 1/args.hertz - (time.time() - start_time)
+                if sleep_time > 0.0:
+                    time.sleep(sleep_time)
+                else:
+                    time.sleep(0)  # yield time to other threads
+            start_time = time.time()
+
+    except KeyboardInterrupt:
+        print('Stopping early.')
+    except Exception as e:
+        print(e)
+        exit(1)
+    finally:
+        if pwm_out_pin is not None:
+            pwm_out_pin.stop()
+        if ttl_out_pin is not None:
+            ttl_out_pin.stop() 
