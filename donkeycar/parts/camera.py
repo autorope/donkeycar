c8fd65835ddbb5a152cc2039f75564ae44d1279a
Ezward <emurmur@yahoo.com>
2021-12-13 22:15:16 +0000

    Added warmup routine to webcam - this guarantees we can produce frames before the camera   is used - also detects if pygame is not installed and prints instructions

--- donkeycar/parts/camera.py
+++ donkeycar/parts/camera.py
@@ -79,7 +79,7 @@ class PiCamera(BaseCamera):
 
 
 class Webcam(BaseCamera):
-    def __init__(self, image_w=160, image_h=120, image_d=3, framerate = 20, iCam = 0):
+    def __init__(self, image_w=160, image_h=120, image_d=3, framerate = 20, camera_index = 0):
         #
         # pygame is not installed by default.  
         # Installation on RaspberryPi (with env activated):
@@ -87,51 +87,74 @@ class Webcam(BaseCamera):
         # sudo apt-get install libsdl2-mixer-2.0-0 libsdl2-image-2.0-0 libsdl2-2.0-0
         # pip install pygame
         #
-        import pygame
-        import pygame.camera
-
         super().__init__()
-        resolution = (image_w, image_h)
-        pygame.init()
-        pygame.camera.init()
-        l = pygame.camera.list_cameras()
-        print('cameras', l)
-        self.cam = pygame.camera.Camera(l[iCam], resolution, "RGB")
-        self.resolution = resolution
-        self.cam.start()
+        self.cam = None
         self.framerate = framerate
 
         # initialize variable used to indicate
         # if the thread should be stopped
         self.frame = None
-        self.on = True
         self.image_d = image_d
+        self.image_w = image_w
+        self.image_h = image_h
 
-        print('WebcamVideoStream loaded.. .warming camera')
+        self.init_camera(image_w, image_h, image_d, camera_index)
+        self.on = True
 
-        time.sleep(2)
+    def init_camera(self, image_w, image_h, image_d, camera_index=0):
+        try:
+            import pygame
+            import pygame.camera
+        except ModuleNotFoundError as e:
+            print("Unabled to import pygame.  Try installing it:")
+            print("    sudo apt-get install libsdl2-mixer-2.0-0 libsdl2-image-2.0-0 libsdl2-2.0-0")
+            print("    pip install pygame")
+            raise e
+
+        print('Opening Webcam...')
+
+        self.resolution = (image_w, image_h)
+        pygame.init()
+        pygame.camera.init()
+        l = pygame.camera.list_cameras()
+        print('cameras', l)
+        try:
+            self.cam = pygame.camera.Camera(l[camera_index], self.resolution, "RGB")
+            self.cam.start()
+
+            print('CSICamera opened.. .warming camera')
+            warming_time = time.time() + 5  # quick after 5 seconds
+            while self.frame is None and time.time() < warming_time:
+                print(".", end="")
+                self.run()
+                time.sleep(0.2)
+            print("")
+
+            if self.frame is None:
+                raise RuntimeError("Unable to start Webcam.")
+        except:
+            raise RuntimeError("Unable to open Webcam.")
+
+    def run(self):
+        import pygame.image
+        if self.cam.query_image():
+            snapshot = self.cam.get_image()
+            snapshot1 = pygame.transform.scale(snapshot, self.resolution)
+            self.frame = pygame.surfarray.pixels3d(pygame.transform.rotate(pygame.transform.flip(snapshot1, True, False), 90))
+            if self.image_d == 1:
+                self.frame = rgb2gray(self.frame)
+        return self.frame
 
     def update(self):
         from datetime import datetime, timedelta
-        import pygame.image
         while self.on:
             start = datetime.now()
-
-            if self.cam.query_image():
-                # snapshot = self.cam.get_image()
-                # self.frame = list(pygame.image.tostring(snapshot, "RGB", False))
-                snapshot = self.cam.get_image()
-                snapshot1 = pygame.transform.scale(snapshot, self.resolution)
-                self.frame = pygame.surfarray.pixels3d(pygame.transform.rotate(pygame.transform.flip(snapshot1, True, False), 90))
-                if self.image_d == 1:
-                    self.frame = rgb2gray(self.frame)
-
+            self.run()
             stop = datetime.now()
             s = 1 / self.framerate - (stop - start).total_seconds()
             if s > 0:
                 time.sleep(s)
 
-        self.cam.stop()
 
     def run_threaded(self):
         return self.frame
@@ -139,7 +162,10 @@ class Webcam(BaseCamera):
     def shutdown(self):
         # indicate that the thread should be stopped
         self.on = False
-        print('stopping Webcam')
+        if self.cam:
+            print('stopping Webcam')
+            self.cam.stop()
+            self.cam = None
         time.sleep(.5)
 
 
