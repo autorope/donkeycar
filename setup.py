a3df79a3573127dff31fc9b2953588056875b214
Ed Murphy <Ezward@users.noreply.github.com>
2022-01-14 12:14:59 -0800

    970 imgaug workaround mock augmentations (#978)
    * Mock ImageAugmentation if imgaug not installed
    - this is a workaround for issue 970
    - https://github.com/autorope/donkeycar/issues/970
    - it is meant to be temporary until we refactor
      the image augmentations to not use imgaug
    
    * Cleanup linting issues
    
    * version="4.3.4"

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='donkeycar',
-      version="4.3.3",
+      version="4.3.4",
       long_description=long_description,
       description='Self driving library for python.',
       url='https://github.com/autorope/donkeycar',
