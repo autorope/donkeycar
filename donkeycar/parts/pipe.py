03b37e97d4f485c24dd0d299ad07d5acacbe7d20
Ezward <ed@junelife.com>
2022-02-12 15:50:23 -0800

    Part the writes inputs to output so they can be renamed.

---
+++ donkeycar/parts/pipe.py
@@ -0,0 +1,17 @@
+from typing import List, Set, Dict, Tuple, Optional
+
+
+class Pipe:
+    """
+    Just pipe all inputs to the output, so they can be renames.
+    """
+    def __init__(self):
+        self.outputs = outputs
+        self.running = True
+
+    def run(self, *args):
+        if self.running:
+            return args
+
+    def shutdown(self):
+        self.running = False
