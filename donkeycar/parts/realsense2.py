93d9bbd45b103ef55a677dfe1d4070855afb2368
Ezward <emurmur@yahoo.com>
2022-03-04 14:59:15 -0800

    T265 driver can take device id - this allows it to be used at the same time as D435 - separated the pose and image streams

--- donkeycar/parts/realsense2.py
+++ donkeycar/parts/realsense2.py
@@ -17,7 +17,8 @@ class RS_T265(object):
     is remarkably consistent.
     '''
 
-    def __init__(self, image_output=False, calib_filename=None):
+    def __init__(self, image_output=False, calib_filename=None, device_id = None):
+        self.device_id = device_id  # serial number of device to use or None to use default
         # Using the image_output will grab two image streams from the fisheye cameras but return only one.
         # This can be a bit much for USB2, but you can try it. Docs recommend USB3 connection for this.
         self.image_output = image_output
@@ -30,17 +31,15 @@ class RS_T265(object):
         print("starting T265")
         self.pipe = rs.pipeline()
         cfg = rs.config()
+        if None != self.device_id:
+            cfg.enable_device(self.device_id)
+
+        # get pose data
         cfg.enable_stream(rs.stream.pose)
         profile = cfg.resolve(self.pipe)
         dev = profile.get_device()
         tm2 = dev.as_tm2()
         
-
-        if self.image_output:
-            #right now it's required for both streams to be enabled
-            cfg.enable_stream(rs.stream.fisheye, 1) # Left camera
-            cfg.enable_stream(rs.stream.fisheye, 2) # Right camera
-
         if calib_filename is not None:
             pose_sensor = tm2.first_pose_sensor()
             self.wheel_odometer = pose_sensor.as_wheel_odometer() 
@@ -57,8 +56,21 @@ class RS_T265(object):
             self.wheel_odometer.load_wheel_odometery_config(chars)   
 
 
-        # Start streaming with requested config
+        # Start streaming pose
         self.pipe.start(cfg)
+
+        # configure image stream from fisheye cameras
+        self.img_pipeline = None
+        if self.image_output:
+            self.img_pipeline = rs.pipeline()
+            cfg = rs.config()
+            if None != self.device_id:
+                cfg.enable_device(self.device_id)
+            cfg.enable_stream(rs.stream.fisheye, 1) # Left camera
+            cfg.enable_stream(rs.stream.fisheye, 2) # Right camera
+            self.img_pipeline.start(cfg)
+
+
         self.running = True
         print("Warning: T265 needs a warmup period of a few seconds before it will emit tracking data.")
         
@@ -67,6 +79,7 @@ class RS_T265(object):
         self.vel = zero_vec
         self.acc = zero_vec
         self.img = None
+        self.img_right = None
 
     def poll(self):
 
@@ -79,28 +92,28 @@ class RS_T265(object):
             self.wheel_odometer.send_wheel_odometry(wo_sensor_id, frame_num, v)
 
         try:
+            # Fetch pose frame from pose stream
             frames = self.pipe.wait_for_frames()
+            pose = frames.get_pose_frame()
+            if pose:
+                data = pose.get_pose_data()
+                self.pos = data.translation
+                self.vel = data.velocity
+                self.acc = data.acceleration
+                logging.debug('realsense pos(%f, %f, %f)' % (self.pos.x, self.pos.y, self.pos.z))
         except Exception as e:
             logging.error(e)
-            return
 
         if self.image_output:
-            #We will just get one image for now.
-            # Left fisheye camera frame
-            left = frames.get_fisheye_frame(1)
-            self.img = np.asanyarray(left.get_data())
-
-
-        # Fetch pose frame
-        pose = frames.get_pose_frame()
-
-        if pose:
-            data = pose.get_pose_data()
-            self.pos = data.translation
-            self.vel = data.velocity
-            self.acc = data.acceleration
-            logging.debug('realsense pos(%f, %f, %f)' % (self.pos.x, self.pos.y, self.pos.z))
-
+            # fetch images from image stream
+            try:
+                img_frames = self.img_pipeline.wait_for_frames()
+                left = img_frames.get_fisheye_frame(1)
+                self.img = np.asanyarray(left.get_data())
+                right = img_frames.get_fisheye_frame(2)
+                self.img_right = right
+            except Exception as e:
+                logging.error(e)
 
     def update(self):
         while self.running:
@@ -108,7 +121,7 @@ class RS_T265(object):
 
     def run_threaded(self, enc_vel_ms):
         self.enc_vel_ms = enc_vel_ms
-        return self.pos, self.vel, self.acc, self.img
+        return self.pos, self.vel, self.acc, self.img, self.img_right
 
     def run(self, enc_vel_ms):
         self.enc_vel_ms = enc_vel_ms
@@ -118,7 +131,13 @@ class RS_T265(object):
     def shutdown(self):
         self.running = False
         time.sleep(0.1)
-        self.pipe.stop()
+        if self.pipe is not None:
+            self.pipe.stop()
+            self.pipe = None
+        if self.img_pipeline is not None:
+            self.img_pipeline.stop()
+            self.img_pipeline = None
+        
 
 
 
