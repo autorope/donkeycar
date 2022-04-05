8eaa7a79134ef04dee29937e2dad7db54210af05
Ezward <ed@junelife.com>
2022-04-05 13:28:56 -0700

    - fix web controller recording state so it is latched. - fix for https://github.com/autorope/donkeycar/issues/996 - version="4.3.10"

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='donkeycar',
-      version="4.3.9",
+      version="4.3.10",
       long_description=long_description,
       description='Self driving library for python.',
       url='https://github.com/autorope/donkeycar',
