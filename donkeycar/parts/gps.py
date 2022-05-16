91f4ee06f96074d54f3d14575c49b530d90f9da9
Ezward <emurmur@yahoo.com>
2022-05-15 22:16:59 -0700

    return early if gps/nmea checksum is wrong - prior to this change it just kept going,   but it should not try to parse a linve   with a back checksum.

--- donkeycar/parts/gps.py
+++ donkeycar/parts/gps.py
@@ -181,7 +181,12 @@ def getGpsPosition(line, debug=False):
     nmea_msg = line[1:-3]      # msg without $ and *## checksum
     nmea_parts = nmea_msg.split(",")
     message = nmea_parts[0]
-    if (message == "GPRMC") or (message == "GNRMC"):   
+
+    if (message == "GPRMC") or (message == "GNRMC"):
+        if True or debug:
+            print()
+            print(line)
+
         #     
         # like '$GPRMC,003918.00,A,3806.92281,N,12235.64362,W,0.090,,060322,,,D*67'
         # GPRMC = Recommended minimum specific GPS/Transit data
@@ -191,6 +196,7 @@ def getGpsPosition(line, debug=False):
         calculated_checksum = calculate_nmea_checksum(line)
         if nmea_checksum != calculated_checksum:
             logger.info(f"NMEA checksum does not match: {nmea_checksum} != {calculated_checksum}")
+            return None
         
         #
         # parse against a known parser to check our parser
