882051ada3867372fc655e8443c12ec06434e92d
Ed Murphy <emurmur@yahoo.com>
2022-05-28 14:37:44 -0700

    version="4.3.17"

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='donkeycar',
-      version="4.3.14",
+      version="4.3.17",
       long_description=long_description,
       description='Self driving library for python.',
       url='https://github.com/autorope/donkeycar',
