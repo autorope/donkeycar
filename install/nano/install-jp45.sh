8701473d35d34dedadbb4d0d65fdf711782229b1
Ed Murphy <Ezward@users.noreply.github.com>
2022-07-06 10:41:21 -0700

    install-jp45.sh enforces that is is run from project folder

--- install/nano/install-jp45.sh
+++ install/nano/install-jp45.sh
@@ -1,2 +1,11 @@
-JP_VERSION=45 ./install.nano.sh
+#!/bin/bash
 
+#
+# must run from within donkey project folder
+#
+if [ ! -d donkeycar ] || [ ! -f setup.py ]; then
+  echo "Error: install-jp45.sh must be run in the donkeycar project folder."
+  exit 255
+fi
+
+JP_VERSION=45 install/nano/install.nano.sh
