cc5e838d8aea36c8b7ed44f1c5447229c4171ff4
DocGarbanzo <dirk.prange@web.de>
2022-11-10 19:34:47 +0000

    Fix kivy imports

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(name='donkeycar',
               'adafruit-circuitpython-rplidar',
               'Jetson.GPIO',
               'matplotlib',
-              'kivy-jetson==2.1',
+              'kivy-jetson',
               'pandas',
               'pyyaml',
               'plotly'
