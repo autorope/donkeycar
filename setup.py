022df76c69a7c72ab3cd041ad177a2f4a966c910
DocGarbanzo <dirk.prange@web.de>
2022-11-27 13:33:41 +0000

    Update version to 4.4.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='donkeycar',
-      version="4.3.25",
+      version="4.4.0",
       long_description=long_description,
       description='Self driving library for python.',
       url='https://github.com/autorope/donkeycar',
