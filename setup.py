988c9ce0a482a61d9af8b13a0f3312143f9bcb56
DocGarbanzo <dirk.prange@web.de>
2022-12-13 09:01:27 +0000

    Fix torchvision to 0.12 to avoid this error https://github.com/pytorch/pytorch/issues/42078.

--- setup.py
+++ setup.py
@@ -89,8 +89,8 @@ setup(name='donkeycar',
           'ci': ['codecov'],
           'tf': ['tensorflow==2.2.0'],
           'torch': [
-              'pytorch>=1.7.1',
-              'torchvision',
+              'pytorch',
+              'torchvision==0.12',
               'torchaudio',
               'fastai'
           ],
