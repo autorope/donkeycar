8420dd8007a70c50adf42d514d6b005eb7c61764
Craig Fox <cfox570@mac.com>
2022-12-31 13:28:00 -0500

    Train script missing transfer parameter

--- donkeycar/templates/train.py
+++ donkeycar/templates/train.py
@@ -4,9 +4,11 @@ Scripts to train a keras model using tensorflow.
 Basic usage should feel familiar: train.py --tubs data/ --model models/mypilot.h5
 
 Usage:
-    train.py [--tubs=tubs] (--model=<model>)
-    [--type=(linear|inferred|tensorrt_linear|tflite_linear)]
+    train.py  [--tubs=tubs] (--model=<model>)
+    [--type=(linear|inferred|tensorrt_linear|tflite_linear)] 
+    [--transfer=<transfer_model>]
     [--comment=<comment>]
+
 
 Options:
     -h --help              Show this screen.
@@ -20,11 +22,15 @@ from donkeycar.pipeline.training import train
 def main():
     args = docopt(__doc__)
     cfg = dk.load_config()
-    tubs = args['--tubs']
+    tubs =  args['--tubs']
+    if tubs == None:
+        tubs = 'data/'
     model = args['--model']
     model_type = args['--type']
+    transfer = args['--transfer']
     comment = args['--comment']
-    train(cfg, tubs, model, model_type, comment)
+    
+    train(cfg, tubs, model, model_type, transfer, comment)
 
 
 if __name__ == "__main__":
