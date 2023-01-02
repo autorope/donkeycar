55a309d803201e2a2e294e6986eafc4d54a4c08f
olivier.bourrasse@renault.com <olivier.bourrasse@renault.com>
2023-01-02 16:39:10 +0100

    added depth avoidance

--- donkeycar/parts/oak_d_camera.py
+++ donkeycar/parts/oak_d_camera.py
@@ -12,16 +12,35 @@ class CameraError(Exception):
     pass
 
 class OakDCamera:
-    def __init__(self, width, height, depth=3, isp_scale=None, framerate=30):
-        self.frame = None
+    def __init__(self, width, height, depth=3, isp_scale=None, framerate=30, enable_depth=False):
+        
         self.on = False
+        
         self.device = None
-        self.queue = None
+        
+        self.queue_xout = None
+        self.queue_xout_depth = None
+        
+        self.frame_xout = None
+        self.frame_xout_depth = None
+        
+        # depth config
+        # Closer-in minimum depth, disparity range is doubled (from 95 to 190):
+        self.extended_disparity = True
+        # Better accuracy for longer distance, fractional disparity 32-levels:
+        self.subpixel = False
+        # Better handling for occlusions:
+        self.lr_check = True
+
         self.latencies = deque([], maxlen=20)
+        self.enable_depth = enable_depth
 
         # Create pipeline
         self.pipeline = dai.Pipeline()
         self.pipeline.setXLinkChunkSize(0) # This might improve reducing the latency on some systems
+
+        if self.enable_depth:
+            self.create_depth_pipeline()
 
         # Define output
         xout = self.pipeline.create(dai.node.XLinkOut)
@@ -70,33 +89,83 @@ class OakDCamera:
         camera.setFps(framerate)
 
         try:
+
             # Connect to device and start pipeline
             logger.info('Starting OAK-D camera')
             self.device = dai.Device(self.pipeline)
-            self.queue = self.device.getOutputQueue(name="xout", maxSize=1, blocking=False)
+
+            if enable_depth:
+                self.queue_xout = self.device.getOutputQueue("xout", maxSize=1, blocking=False)
+                self.queue_xout_depth = self.device.getOutputQueue("xout_depth", maxSize=1, blocking=False)
+            else:
+                self.queue_xout = self.device.getOutputQueue("xout", maxSize=1, blocking=False)
+                self.queue_xout_depth = None
 
             # Get the first frame or timeout
             warming_time = time.time() + 5  # seconds
-            while self.frame is None and time.time() < warming_time:
+            while self.frame_xout is None and time.time() < warming_time:
                 logger.info("...warming camera")
                 self.run()
                 time.sleep(0.2)
 
-            if self.frame is None:
+            if self.frame_xout is None:
                 raise CameraError("Unable to start OAK-D camera.")
 
-            logger.info("OAK-D camera ready.")
             self.on = True
+            logger.info("OAK-D camera ready.")
+            
         except:
             self.shutdown()
             raise
+
+    def create_depth_pipeline(self):
         
+        # Create depth nodes
+        monoRight = self.pipeline.create(dai.node.MonoCamera)
+        monoLeft = self.pipeline.create(dai.node.MonoCamera)
+        
+        stereo_manip = self.pipeline.create(dai.node.ImageManip)
+        stereo = self.pipeline.create(dai.node.StereoDepth)
+
+        # Better handling for occlusions:
+        stereo.setLeftRightCheck(True)
+        # Closer-in minimum depth, disparity range is doubled:
+        stereo.setExtendedDisparity(True)
+        # Better accuracy for longer distance, fractional disparity 32-levels:
+        stereo.setSubpixel(False)
+        stereo.initialConfig.setMedianFilter(dai.MedianFilter.KERNEL_7x7)
+        stereo.initialConfig.setConfidenceThreshold(200)
+
+        xout_depth = self.pipeline.create(dai.node.XLinkOut)
+        xout_depth.setStreamName("xout_depth")
+
+        # Crop range
+        topLeft = dai.Point2f(0.1875, 0.5)
+        bottomRight = dai.Point2f(0.8125, 0.75)
+
+        # Properties
+        monoRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
+        monoLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
+        monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+        monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+
+        stereo_manip.initialConfig.setCropRect(topLeft.x, topLeft.y, bottomRight.x, bottomRight.y)
+        # manip.setMaxOutputFrameSize(monoRight.getResolutionHeight()*monoRight.getResolutionWidth()*3)
+        stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
+
+        # Linking
+        # configIn.out.link(manip.inputConfig)
+        monoRight.out.link(stereo.right)
+        monoLeft.out.link(stereo.left)
+        stereo.depth.link(stereo_manip.inputImage)
+        stereo_manip.out.link(xout_depth.input)
+
     def run(self):
+
         # Grab the frame from the stream 
-        if self.queue is not None:
-            data = self.queue.get() # blocking
-            image_data = data.getFrame()
-            self.frame = np.moveaxis(image_data, 0, -1)
+        if self.queue_xout is not None:
+            data_xout = self.queue_xout.get() # blocking
+            self.frame_xout = data_xout.getFrame()
 
             if logger.isEnabledFor(logging.DEBUG):
                 # Latency in miliseconds 
@@ -106,10 +175,18 @@ class OakDCamera:
                         .format(self.latencies[-1], np.average(self.latencies), np.std(self.latencies)))
                     self.latencies.clear()
 
-        return self.frame
+        if self.queue_xout_depth is not None:
+            data_xout_depth = self.queue_xout_depth.get()
+            self.frame_xout_depth = data_xout_depth.getFrame()
+
+
+        # return self.frame
 
     def run_threaded(self):
-        return self.frame
+        if self.enable_depth:
+            return self.frame_xout,self.frame_xout_depth
+        else:
+            return self.frame_xout
 
     def update(self):
         # Keep looping infinitely until the thread is stopped
