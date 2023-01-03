4910f54a6d3e42e2bb9afdaf4a22f70718375b2f
olivier.bourrasse@renault.com <olivier.bourrasse@renault.com>
2023-01-03 20:08:02 +0100

    Changed to 50px top patch

--- donkeycar/parts/depth_avoidance.py
+++ donkeycar/parts/depth_avoidance.py
@@ -124,31 +124,35 @@ class DepthAvoidance:
         self.throttle = throttle
         self.steering_angle = 0
 
-        obstacle_detected_in_PCLC = ((depth_frame[50:100,100:200] < self.CLOSE_DISTANCE_MM).sum() > self.NB_PIXELS_IN_RANGE)
-        obstacle_detected_in_PCRC = ((depth_frame[50:100,200:300] < self.CLOSE_DISTANCE_MM).sum() > self.NB_PIXELS_IN_RANGE)
+        # obstacle_detected_in_PCLC = ((depth_frame[50:100,100:200] < self.CLOSE_DISTANCE_MM).sum() > self.NB_PIXELS_IN_RANGE)
+        # obstacle_detected_in_PCRC = ((depth_frame[50:100,200:300] < self.CLOSE_DISTANCE_MM).sum() > self.NB_PIXELS_IN_RANGE)
+        obstacle_detected_in_PCLC = ((depth_frame[0:50,100:200] < self.CLOSE_DISTANCE_MM).sum() > self.NB_PIXELS_IN_RANGE)
+        obstacle_detected_in_PCRC = ((depth_frame[0:50,200:300] < self.CLOSE_DISTANCE_MM).sum() > self.NB_PIXELS_IN_RANGE)
         
         if obstacle_detected_in_PCLC or obstacle_detected_in_PCRC:
             self.emergency_brake = obstacle_detected_in_PCLC & obstacle_detected_in_PCRC
             self.steering_angle += obstacle_detected_in_PCLC * self.FULL_RIGHT_STEERING
             self.steering_angle += obstacle_detected_in_PCRC * self.FULL_LEFT_STEERING
         else:    
-            obstacle_detected_in_PCLL = ((depth_frame[50:100,0:100] < self.CLOSE_DISTANCE_MM).sum() > self.NB_PIXELS_IN_RANGE)
+            # obstacle_detected_in_PCLL = ((depth_frame[50:100,0:100] < self.CLOSE_DISTANCE_MM).sum() > self.NB_PIXELS_IN_RANGE)
+            obstacle_detected_in_PCLL = ((depth_frame[0:50,0:100] < self.CLOSE_DISTANCE_MM).sum() > self.NB_PIXELS_IN_RANGE)
             self.steering_angle += obstacle_detected_in_PCLL * self.THREE_QUARTER_RIGHT_STEERING
             
-            obstacle_detected_in_PCRR = ((depth_frame[50:100,300:400] < self.CLOSE_DISTANCE_MM).sum() > self.NB_PIXELS_IN_RANGE)
+            # obstacle_detected_in_PCRR = ((depth_frame[50:100,300:400] < self.CLOSE_DISTANCE_MM).sum() > self.NB_PIXELS_IN_RANGE)
+            obstacle_detected_in_PCRR = ((depth_frame[0:50,300:400] < self.CLOSE_DISTANCE_MM).sum() > self.NB_PIXELS_IN_RANGE)
             self.steering_angle += obstacle_detected_in_PCRR * self.THREE_QUARTER_LEFT_STEERING
             
-            obstacle_detected_in_PFLC = ((depth_frame[0:50,100:200] < self.FAR_DISTANCE_MM).sum() > self.NB_PIXELS_IN_RANGE)
-            self.steering_angle += obstacle_detected_in_PFLC * self.HALF_RIGHT_STEERING
+            # obstacle_detected_in_PFLC = ((depth_frame[0:50,100:200] < self.FAR_DISTANCE_MM).sum() > self.NB_PIXELS_IN_RANGE)
+            # self.steering_angle += obstacle_detected_in_PFLC * self.HALF_RIGHT_STEERING
             
-            obstacle_detected_in_PFRC = ((depth_frame[0:50,200:300] < self.FAR_DISTANCE_MM).sum() > self.NB_PIXELS_IN_RANGE)
-            self.steering_angle += obstacle_detected_in_PFRC * self.HALF_LEFT_STEERING
+            # obstacle_detected_in_PFRC = ((depth_frame[0:50,200:300] < self.FAR_DISTANCE_MM).sum() > self.NB_PIXELS_IN_RANGE)
+            # self.steering_angle += obstacle_detected_in_PFRC * self.HALF_LEFT_STEERING
             
-            obstacle_detected_in_PFLL = ((depth_frame[0:50,0:100] < self.FAR_DISTANCE_MM).sum() > self.NB_PIXELS_IN_RANGE)
-            self.steering_angle += obstacle_detected_in_PFLL * self.QUARTER_RIGHT_STEERING
+            # obstacle_detected_in_PFLL = ((depth_frame[0:50,0:100] < self.FAR_DISTANCE_MM).sum() > self.NB_PIXELS_IN_RANGE)
+            # self.steering_angle += obstacle_detected_in_PFLL * self.QUARTER_RIGHT_STEERING
             
-            obstacle_detected_in_PFRR = ((depth_frame[0:50,300:400] < self.FAR_DISTANCE_MM).sum() > self.NB_PIXELS_IN_RANGE)
-            self.steering_angle += obstacle_detected_in_PFRR * self.QUARTER_LEFT_STEERING
+            # obstacle_detected_in_PFRR = ((depth_frame[0:50,300:400] < self.FAR_DISTANCE_MM).sum() > self.NB_PIXELS_IN_RANGE)
+            # self.steering_angle += obstacle_detected_in_PFRR * self.QUARTER_LEFT_STEERING
 
         self.steering_angle = self.clamp(self.steering_angle, -1, 1)
         if self.steering_angle == 0:
