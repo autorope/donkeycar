d0f6feadba95fd335d3acf1dbb9213f19e8cd498
Ezward <ed@junelife.com>
2023-01-15 12:10:54 -0800

    Update cv_control template to use standard parts - uses methods in complete.py to add cameras, joystick,   web controller, and drive trains. - update doc opts and the way the template launches to   be more standard.

--- donkeycar/templates/cv_control.py
+++ donkeycar/templates/cv_control.py
@@ -1,27 +1,34 @@
 #!/usr/bin/env python3
 """
 
+Scripts to drive on autopilot using computer vision
+
 Usage:
-    manage.py --name=your_name [--debug]
+    manage.py (drive) [--js] [--log=INFO] [--camera=(single|stereo)] [--myconfig=<filename>]
 
 
 Options:
-    -h --help          Show this screen.    
+    -h --help          Show this screen.
+    --js               Use physical joystick.
+    --myconfig=filename     Specify myconfig file to use.
+                            [default: myconfig.py]
 """
-import os
-import time
+import logging
 
 from docopt import docopt
-import numpy as np
-import cv2
-from simple_pid import PID
 
 import donkeycar as dk
 from donkeycar.parts.datastore import TubHandler
 from donkeycar.parts.line_follower import LineFollower
+from donkeycar.templates.complete import add_odometry, add_camera, \
+    add_user_controller, add_drivetrain, add_simulator, add_imu
+from donkeycar.parts.logger import LoggerPart
+from donkeycar.parts.transform import Lambda
+from donkeycar.parts.explode import ExplodeDict
+from donkeycar.parts.controller import JoystickController
 
 
-def drive(cfg, args):
+def drive(cfg, use_joystick=False, camera_type='single'):
     '''
     Construct a working robotic vehicle from many parts.
     Each part runs as a job in the Vehicle loop, calling either
@@ -35,46 +42,69 @@ def drive(cfg, args):
     #Initialize car
     V = dk.vehicle.Vehicle()
 
-    #Camera
-    if cfg.DONKEY_GYM:
-        from donkeycar.parts.dgym import DonkeyGymEnv
-        cfg.GYM_CONF['racer_name'] = args['--name']
-        cfg.GYM_CONF['car_name'] = args['--name']
-        cam = DonkeyGymEnv(cfg.DONKEY_SIM_PATH, host=cfg.SIM_HOST, env_name=cfg.DONKEY_GYM_ENV_NAME, conf=cfg.GYM_CONF, delay=cfg.SIM_ARTIFICIAL_LATENCY)
-        inputs = ['steering', 'throttle']
-    else:
-        from donkeycar.parts.camera import PiCamera
-        cam = PiCamera(image_w=cfg.IMAGE_W, image_h=cfg.IMAGE_H, image_d=cfg.IMAGE_DEPTH)
-        inputs = []
+    #
+    # setup primary camera
+    #
+    add_camera(V, cfg, camera_type)
 
-    V.add(cam, inputs=inputs, outputs=['cam/image_array'], threaded=True)
-        
-    #Controller
-    V.add(LineFollower(bool(args['--debug'])), 
+    #
+    # add the user input controller(s)
+    # - this will add the web controller
+    # - it will optionally add any configured 'joystick' controller
+    #
+    has_input_controller = hasattr(cfg, "CONTROLLER_TYPE") and cfg.CONTROLLER_TYPE != "mock"
+    ctr = add_user_controller(V, cfg, use_joystick, input_image = 'map/image')
+
+    #
+    # explode the web buttons into their own key/values in memory
+    #
+    V.add(ExplodeDict(V.mem, "web/"), inputs=['web/buttons'])
+
+    #
+    # track user vs autopilot condition
+    #
+    class UserCondition:
+        def run(self, mode):
+            if mode == 'user':
+                return True
+            else:
+                return False
+    V.add(UserCondition(), inputs=['user/mode'], outputs=['run_user'])
+
+    #See if we should even run the pilot module.
+    #This is only needed because the part run_condition only accepts boolean
+    class PilotCondition:
+        def run(self, mode):
+            if mode == 'user':
+                return False
+            else:
+                return True
+
+    V.add(PilotCondition(), inputs=['user/mode'], outputs=['run_pilot'])
+
+
+    #
+    # Computer Vision Controller
+    #
+    V.add(LineFollower(False),
           inputs=['cam/image_array'],
-          outputs=['steering', 'throttle', 'recording'])
+          outputs=['steering', 'throttle', 'recording'],
+          run_condition="run_pilot")
 
         
-    #Drive train setup
-    if not cfg.DONKEY_GYM:
-        from donkeycar.parts.actuator import PCA9685, PWMSteering, PWMThrottle
+    #
+    # Setup drivetrain
+    #
+    add_drivetrain(V, cfg)
 
-        steering_controller = PCA9685(cfg.STEERING_CHANNEL, cfg.PCA9685_I2C_ADDR, busnum=cfg.PCA9685_I2C_BUSNUM)
-        steering = PWMSteering(controller=steering_controller,
-                                        left_pulse=cfg.STEERING_LEFT_PWM, 
-                                        right_pulse=cfg.STEERING_RIGHT_PWM)
-        
-        throttle_controller = PCA9685(cfg.THROTTLE_CHANNEL, cfg.PCA9685_I2C_ADDR, busnum=cfg.PCA9685_I2C_BUSNUM)
-        throttle = PWMThrottle(controller=throttle_controller,
-                                        max_pulse=cfg.THROTTLE_FORWARD_PWM,
-                                        zero_pulse=cfg.THROTTLE_STOPPED_PWM, 
-                                        min_pulse=cfg.THROTTLE_REVERSE_PWM)
+    # Print Joystick controls
+    if ctr is not None and isinstance(ctr, JoystickController):
+        ctr.print_controls()
 
-        V.add(steering, inputs=['steering'])
-        V.add(throttle, inputs=['throttle'])
-    
-    #add tub to save data
 
+    #
+    # add tub to save data
+    #
     inputs=['cam/image_array',
             'steering', 'throttle']
 
@@ -85,13 +115,22 @@ def drive(cfg, args):
     tub = th.new_tub_writer(inputs=inputs, types=types)
     V.add(tub, inputs=inputs, outputs=["tub/num_records"], run_condition="recording")
 
-    #run the vehicle
+    #
+    # run the vehicle
+    #
     V.start(rate_hz=cfg.DRIVE_LOOP_HZ, 
             max_loop_count=cfg.MAX_LOOPS)
 
 
 if __name__ == '__main__':
     args = docopt(__doc__)
-    cfg = dk.load_config()
-    drive(cfg, args)
-    
+    cfg = dk.load_config(myconfig=args['--myconfig'])
+
+    log_level = args['--log'] or "INFO"
+    numeric_level = getattr(logging, log_level.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % log_level)
+    logging.basicConfig(level=numeric_level)
+
+    if args['drive']:
+        drive(cfg, use_joystick=args['--js'], camera_type=args['--camera'])
