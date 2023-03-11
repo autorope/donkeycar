faff54c538234d86d2e92542aeea2cf144623479
olivier.bourrasse@renault.com <olivier.bourrasse@renault.com>
2023-03-11 11:45:13 +0100

    fix

--- donkeycar/parts/interpreter.py
+++ donkeycar/parts/interpreter.py
@@ -425,8 +425,8 @@ class OnnxInterpreter(Interpreter):
 
         img_arr = np.expand_dims(img_arr, axis=0).astype(np.float32)
 
-        if type(other_arr) != 'NoneType':
-            
+        # if type(other_arr) != 'NoneType':
+        if type(other_arr) == 'numpy.ndarray':  
             other_arr = np.expand_dims(other_arr, axis=0).astype(np.float32)
             outputs_inference = self.ort_sess.run(self.out_name, {self.in_names[0]: img_arr, self.in_names[1]: other_arr })
         else:
