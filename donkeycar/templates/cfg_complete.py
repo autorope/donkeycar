8fbd091cba4f29cecdd0c6733bf2539372224a9e
olivier.bourrasse@renault.com <olivier.bourrasse@renault.com>
2023-04-23 15:05:33 +0200

    Added steering_throttle_scaler Fixed cfg_complete

--- donkeycar/templates/cfg_complete.py
+++ donkeycar/templates/cfg_complete.py
@@ -85,6 +85,14 @@ OBSTACLE_DETECTOR_BEHAVIOR_LIST = ['NA', 'left', 'middle', 'right']
 BEHAVIOR_LIST = ['left', 'right']
 OBSTACLE_DETECTOR_AVOIDANCE_ENABLED = False # To free drive using behavior model
 
+# STEERING_THROTTLE_SCALER
+# scaled_throttle = throttle * THROTTLE_FACTOR
+# scaled_steering = steering_angle * STEERING_FACTOR * STEERING_ON_THROTTLE_FACTOR * THROTTLE_FACTOR
+STEERING_THROTTLE_SCALER_ENABLED = True
+STEERING_FACTOR = 1.0
+THROTTLE_FACTOR = 1.0
+STEERING_ON_THROTTLE_FACTOR = 1.0
+
 #CAMERA Settings Vivatech 2022 (nano)
 #CAMERA_TYPE = "CSIC"   # (PICAM|WEBCAM|CVCAM|CSIC|V4L|D435|MOCK|IMAGE_LIST)
 #IMAGE_W = 160
@@ -488,7 +496,7 @@ USE_JOYSTICK_AS_DEFAULT = False      #when starting the manage.py, when True, wi
 JOYSTICK_MAX_THROTTLE = 0.5         #this scalar is multiplied with the -1 to 1 throttle value to limit the maximum throttle. This can help if you drop the controller or just don't need the full speed available.
 JOYSTICK_STEERING_SCALE = 1.0       #some people want a steering that is less sensitve. This scalar is multiplied with the steering -1 to 1. It can be negative to reverse dir.
 AUTO_RECORD_ON_THROTTLE = True      #if true, we will record whenever throttle is not zero. if false, you must manually toggle recording with some other trigger. Usually circle button on joystick.
-CONTROLLER_TYPE = 'xbox'            #(ps3|ps4|xbox|pigpio_rc|nimbus|wiiu|F710|rc3|MM1|custom) custom will run the my_joystick.py controller written by the `donkey createjs` command
+CONTROLLER_TYPE = 'mock'            #(ps3|ps4|xbox|pigpio_rc|nimbus|wiiu|F710|rc3|MM1|custom) custom will run the my_joystick.py controller written by the `donkey createjs` command
 USE_NETWORKED_JS = False            #should we listen for remote joystick control over the network?
 NETWORK_JS_SERVER_IP = None         #when listening for network joystick control, which ip is serving this information
 JOYSTICK_DEADZONE = 0.01            # when non zero, this is the smallest throttle before recording triggered.
