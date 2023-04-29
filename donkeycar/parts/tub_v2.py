e5cede7b9de805845724abf9d0e0b4be8454f40d
olivier.bourrasse@renault.com <olivier.bourrasse@renault.com>
2023-04-29 20:55:23 +0200

    fix

--- donkeycar/parts/tub_v2.py
+++ donkeycar/parts/tub_v2.py
@@ -60,7 +60,7 @@ class Tub(object):
                     # Handle image array
                     ### image = Image.fromarray(np.uint8(value))
                     name = Tub._image_file_name(self.manifest.current_index, key)
-                    ### image_path = os.path.join(self.images_base_path, name)
+                    image_path = os.path.join(self.images_base_path, name)
                     ### image.save(image_path)
                     contents[key] = name
                 elif input_type == 'gray16_array':
