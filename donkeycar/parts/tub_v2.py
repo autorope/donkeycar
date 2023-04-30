0ba090316c305f1c30ffc4ec983714b9f713132f
olivier.bourrasse@renault.com <olivier.bourrasse@renault.com>
2023-04-30 12:35:11 +0200

    fix

--- donkeycar/parts/tub_v2.py
+++ donkeycar/parts/tub_v2.py
@@ -78,7 +78,7 @@ class Tub(object):
 
                     # Write binary
                     with open(image_path, "wb") as binary_file:
-                        binary_file.write(np.uint8(value))
+                        binary_file.write(value.tobytes())
 
                     # Try fire and forget asyncio
                     # common part
