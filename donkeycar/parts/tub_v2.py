7bf0636010654be2b6ebcb7b52eb2ee48afef60c
olivier.bourrasse@renault.com <olivier.bourrasse@renault.com>
2023-05-03 18:30:59 +0200

    fix

--- donkeycar/parts/tub_v2.py
+++ donkeycar/parts/tub_v2.py
@@ -66,11 +66,11 @@ class Tub(object):
                 elif input_type == 'image_array':
                     # Handle image array
                     # original version
-                    # image = Image.fromarray(np.uint8(value))
                     name = Tub._image_file_name(self.manifest.current_index, key)
                     image_path = os.path.join(self.images_base_path, name)
-                    # image.save(image_path)
-                    
+                    with Image.fromarray(np.uint8(value)) as image:
+                        image.save(image_path)
+
                     # zmq version
                     # key = image_path
                     # message = key.encode() + b" " + np.uint8(value).tobytes()
