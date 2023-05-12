80fb88f47c9be65366f2453910783eee0dddafe8
btrinite <btrinite@free.fr>
2023-05-12 10:57:15 +0200

    remove old class

--- donkeycar/parts/steering_throttle_scaler.py
+++ donkeycar/parts/steering_throttle_scaler.py
@@ -25,21 +25,3 @@ class SteeringThrottleScaler:
     def shutdown(self):
         self.running = False
         logger.info('Stopping Steering Throttle Scaler')
-
-class SteeringThrottleScaler:
-    def __init__(self, steering_factor, throttle_factor, steering_on_throttle_factor):
-       
-        self.steering_factor = steering_factor
-        self.throttle_factor = throttle_factor
-        self.steering_on_throttle_factor = steering_on_throttle_factor
-        
-        self.running = True    
-
-    def run(self, steering_angle, throttle):
-        scaled_throttle = throttle * self.throttle_factor
-        scaled_steering = steering_angle * self.steering_factor * self.steering_on_throttle_factor * self.throttle_factor
-        return scaled_steering, scaled_throttle
-        
-    def shutdown(self):
-        self.running = False
-        logger.info('Stopping Steering Throttle Scaler')
