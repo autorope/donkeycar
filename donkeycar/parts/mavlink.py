3c8e62900ba57d2a8ca96482158b456cdfd53c1f
Chris Anderson <analoguedevices@gmail.com>
2023-08-01 15:53:18 -0700

    Fixed polling

--- donkeycar/parts/mavlink.py
+++ donkeycar/parts/mavlink.py
@@ -1,12 +1,25 @@
-from pymavlink import mavutil
 import time
 import math
+
+debug = False
+
+try:
+    import serial
+except ImportError:
+    print("PySerial not found.  Please install: pip install pyserial")
+
+try:
+    from pymavlink import mavutil
+except ImportError:
+    print("Pymavlink not found.  Please install: pip install pymavlink")
+
 
 class MavlinkIMUPart:
     def __init__(self, connection_string):
         # Create the connection to Pixhawk
-        self.master = mavutil.mavlink_connection(connection_string)
+        self.master = mavutil.mavlink_connection(connection_string, baud=115200)
         self.master.wait_heartbeat()
+        print("Heartbeat from system (system %u component %u)" % (self.master.target_system, self.master.target_component))
         
         # IMU data
         self.roll = 0
@@ -31,25 +44,27 @@ class MavlinkIMUPart:
         self.poll()
 
     def poll(self):
-        while self.running:
-            attitude_msg = self.master.recv_match(type='ATTITUDE', blocking=True)
-            position_msg = self.master.recv_match(type='GLOBAL_POSITION_INT', blocking=True)
-            velocity_msg = self.master.recv_match(type='VFR_HUD', blocking=True)
+        if self.running:
+            message = self.master.recv_match()
+            if message is not None:
+                attitude_msg = self.master.recv_match(type='ATTITUDE', blocking=True)
+                position_msg = self.master.recv_match(type='GLOBAL_POSITION_INT', blocking=True)
+                velocity_msg = self.master.recv_match(type='VFR_HUD', blocking=True)
 
-            # Updating IMU data
-            self.roll = attitude_msg.roll
-            self.pitch = attitude_msg.pitch
-            self.yaw = attitude_msg.yaw
-            
-            # Updating Position data
-            self.lat = position_msg.lat / 1e7  # Convert to degrees
-            self.lon = position_msg.lon / 1e7  # Convert to degrees
-            self.alt = position_msg.alt / 1e3  # Convert to meters
+                # Updating IMU data
+                self.roll = attitude_msg.roll
+                self.pitch = attitude_msg.pitch
+                self.yaw = attitude_msg.yaw
+                
+                # Updating Position data
+                self.lat = position_msg.lat / 1e7  # Convert to degrees
+                self.lon = position_msg.lon / 1e7  # Convert to degrees
+                self.alt = position_msg.alt / 1e3  # Convert to meters
 
-            # Updating Velocity data (VFR_HUD provides ground speeds)
-            self.vx = velocity_msg.groundspeed * math.cos(self.yaw)
-            self.vy = velocity_msg.groundspeed * math.sin(self.yaw)
-            self.vz = velocity_msg.climb
+                # Updating Velocity data (VFR_HUD provides ground speeds)
+                self.vx = velocity_msg.groundspeed * math.cos(self.yaw)
+                self.vy = velocity_msg.groundspeed * math.sin(self.yaw)
+                self.vz = velocity_msg.climb
 
     def run_threaded(self):
         return (self.roll, self.pitch, self.yaw, 
