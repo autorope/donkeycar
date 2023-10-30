1b77a8cde9a7a035795f66c6a027557adcf83b1d
Romain Marret <romain.marret@renault.com>
2023-10-30 17:28:41 +0100

    oak-d refacto

--- donkeycar/parts/oak_d_camera.py
+++ donkeycar/parts/oak_d_camera.py
@@ -132,118 +132,56 @@ class OakDCamera:
                  rgb_exposure_time = 2000,
                  rgb_sensor_iso = 1200,
                  rgb_wb_manual= 2800):
+        
+        self.width = width
+        self.height = height
+        self.depth = depth
+        self.isp_scale = isp_scale
+        self.framerate = framerate
+        self.enable_depth = enable_depth
+        self.enable_obstacle_dist = enable_obstacle_dist
+        self.rgb_resolution = rgb_resolution
+        self.rgb_apply_cropping = rgb_apply_cropping
+        self.rgb_sensor_crop_x = rgb_sensor_crop_x
+        self.rgb_sensor_crop_y = rgb_sensor_crop_y
+        self.rgb_video_size = rgb_video_size
+        self.rgb_apply_manual_conf = rgb_apply_manual_conf
+        self.rgb_exposure_time = rgb_exposure_time
+        self.rgb_sensor_iso = rgb_sensor_iso
+        self.rgb_wb_manual = rgb_wb_manual
 
+        # depth config
+        self.extended_disparity = True # Closer-in minimum depth, disparity range is doubled (from 95 to 190)
+        self.subpixel = False # Better accuracy for longer distance, fractional disparity 32-levels
+        self.lr_check = True # Better handling for occlusions
         
         self.on = False
-        
         self.device = None
-
-        self.rgb_resolution = rgb_resolution
-        
         self.queue_xout = None
         self.queue_xout_depth = None
         self.queue_xout_spatial_data = None
-        self.roi_distances = []
-
         self.frame_xout = None
+        self.frame_time = None
         self.frame_xout_depth = None
-        
-        # depth config
-        # Closer-in minimum depth, disparity range is doubled (from 95 to 190):
-        self.extended_disparity = True
-        # Better accuracy for longer distance, fractional disparity 32-levels:
-        self.subpixel = False
-        # Better handling for occlusions:
-        self.lr_check = True
-
+        self.roi_distances = []
         self.latencies = deque([], maxlen=20)
-        self.enable_depth = enable_depth
-        self.enable_obstacle_dist = enable_obstacle_dist
 
         # Create pipeline
         self.pipeline = dai.Pipeline()
         # self.pipeline.setCameraTuningBlobPath('/tuning_color_ov9782_wide_fov.bin')
         self.pipeline.setXLinkChunkSize(0) # This might improve reducing the latency on some systems
 
-        if self.enable_depth:
-            self.create_depth_pipeline()
-
-        if self.enable_obstacle_dist:
-            self.create_obstacle_dist_pipeline()
-
-        # Define output
-        xout = self.pipeline.create(dai.node.XLinkOut)
-        xout.setStreamName("xout")
-
-        # Define a source and Link
-        if depth == 3:
-            # Source
-            camera = self.pipeline.create(dai.node.ColorCamera)
-            if self.rgb_resolution == "800p":
-                camera.setResolution(dai.ColorCameraProperties.SensorResolution.THE_800_P)
-            elif self.rgb_resolution == "1080p":
-                camera.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
-            else:
-                camera.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
-            camera.setInterleaved(False)
-            camera.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
-
-            if isp_scale:
-                # see https://docs.google.com/spreadsheets/d/153yTstShkJqsPbkPOQjsVRmM8ZO3A6sCqm7uayGF-EE/edit#gid=0
-                camera.setIspScale(isp_scale) # "scale" sensor size, (9,19) = 910x512 ; seems very slightly faster
-            
-            if rgb_apply_cropping:
-                camera.setSensorCrop(rgb_sensor_crop_x, rgb_sensor_crop_y) # When croping to keep only smaller video
-
-                camera.setVideoSize(rgb_video_size) # Desired video size = ispscale result or smaller if croping
-
-
-            # Resize image
-            camera.setPreviewKeepAspectRatio(False)
-            camera.setPreviewSize(width, height) # wich means cropping if aspect ratio kept
-            
-            camera.setIspNumFramesPool(1)
-            camera.setVideoNumFramesPool(1)
-            camera.setPreviewNumFramesPool(1)
-
-            if rgb_apply_manual_conf:
-                camera.initialControl.setManualExposure(rgb_exposure_time, rgb_sensor_iso)
-                camera.initialControl.setManualWhiteBalance(rgb_wb_manual)
-            
-                # camRgb.initialControl.setSharpness(0) 
-                # camRgb.initialControl.setLumaDenoise(0)
-                # camRgb.initialControl.setChromaDenoise(4)
-            else:
-
-                camera.initialControl.SceneMode(dai.CameraControl.SceneMode.SPORTS)
-                camera.initialControl.setAutoWhiteBalanceMode(dai.CameraControl.AutoWhiteBalanceMode.AUTO)
-    
-            # Link
-            camera.preview.link(xout.input)
-
-        elif depth == 1:
-            # Source
-            camera = self.pipeline.create(dai.node.MonoCamera)
-            camera.setBoardSocket(dai.CameraBoardSocket.LEFT)
-            camera.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
-
-            # Resize image
-            manip = self.pipeline.create(dai.node.ImageManip)
-            manip.setMaxOutputFrameSize(width * height)
-            manip.initialConfig.setResize(width, height)
-            manip.initialConfig.setFrameType(dai.RawImgFrame.Type.GRAY8)
-
-            # Link
-            camera.out.link(manip.inputImage)
-            manip.out.link(xout.input)
-
+        if self.depth == 3:
+            self.create_color_pipeline()
+        elif self.depth == 1:
+            self.create_mono_pipeline()
         else:
             raise ValueError("'depth' parameter must be either '3' (RGB) or '1' (GRAY)")
-
-        # Common settings
-        camera.initialControl.setManualFocus(0) # from calibration data
-        camera.initialControl.setAutoWhiteBalanceMode(dai.CameraControl.AutoWhiteBalanceMode.FLUORESCENT) # CLOUDY_DAYLIGHT FLUORESCENT
-        camera.setFps(framerate)
+        
+        if self.enable_depth:
+            self.create_depth_pipeline()
+        elif self.enable_obstacle_dist:
+            self.create_obstacle_dist_pipeline()
 
         try:
 
@@ -251,59 +189,99 @@ class OakDCamera:
             logger.info('Starting OAK-D camera')
             self.device = dai.Device(self.pipeline)
 
-            warming_time = time.time() + 5  # seconds
-                
+            # Create queues
+            self.queue_xout = self.device.getOutputQueue("xout", maxSize=1, blocking=False)
             if enable_depth:
-                self.queue_xout = self.device.getOutputQueue("xout", maxSize=1, blocking=False)
                 self.queue_xout_depth = self.device.getOutputQueue("xout_depth", maxSize=1, blocking=False)
-            
-                # Get the first frame or timeout
-                while (self.frame_xout is None or self.frame_xout_depth is None) and time.time() < warming_time:
-                    logger.info("...warming RGB and depth cameras")
-                    self.run()
-                    time.sleep(0.2)
-
-                if self.frame_xout is None:
-                    raise CameraError("Unable to start OAK-D RGB and Depth camera.")
-
             elif enable_obstacle_dist:
-                self.queue_xout = self.device.getOutputQueue("xout", maxSize=1, blocking=False)
                 self.queue_xout_spatial_data = self.device.getOutputQueue("spatialData", maxSize=1, blocking=False)
             
-            else:
-                self.queue_xout = self.device.getOutputQueue("xout", maxSize=1, blocking=False)
-                self.queue_xout_depth = None
+            # Get the first frame or timeout
+            warming_time = time.time() + 5  # seconds
+            while self.frame_xout is None and time.time() < warming_time:
+                logger.info("...warming camera")
+                self.run()
+                time.sleep(0.2)
 
-                # Get the first frame or timeout
-                while self.frame_xout is None and time.time() < warming_time:
-                    logger.info("...warming camera")
-                    self.run()
-                    time.sleep(0.2)
-
-                if self.frame_xout is None:
-                    raise CameraError("Unable to start OAK-D camera.")
+            if self.frame_xout is None:
+                raise CameraError("Unable to start OAK-D camera.")
 
             self.on = True
             logger.info("OAK-D camera ready.")
-            
         except:
             self.shutdown()
             raise
 
-    def create_depth_pipeline(self):
+    def create_color_pipeline(self):
+        # Source
+        camera = self.pipeline.create(dai.node.ColorCamera)
+        camera.setFps(self.framerate)
+        if self.rgb_resolution == "800p":
+            camera.setResolution(dai.ColorCameraProperties.SensorResolution.THE_800_P)
+        elif self.rgb_resolution == "1080p":
+            camera.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+        else:
+            camera.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+        camera.setInterleaved(False)
+        camera.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
+
+        if self.isp_scale:
+            # see https://docs.google.com/spreadsheets/d/153yTstShkJqsPbkPOQjsVRmM8ZO3A6sCqm7uayGF-EE/edit#gid=0
+            camera.setIspScale(self.isp_scale)
         
+        if self.rgb_apply_cropping:
+            camera.setSensorCrop(self.rgb_sensor_crop_x, self.rgb_sensor_crop_y) # When croping to keep only smaller video
+            camera.setVideoSize(self.rgb_video_size) # Desired video size = ispscale result or smaller if croping
+
+        # Resize image
+        camera.setPreviewKeepAspectRatio(False)
+        camera.setPreviewSize(self.width, self.height) # wich means cropping if aspect ratio kept
+        
+        camera.setIspNumFramesPool(1)
+        camera.setVideoNumFramesPool(1)
+        camera.setPreviewNumFramesPool(1)
+
+        if self.rgb_apply_manual_conf:
+            camera.initialControl.setManualExposure(self.rgb_exposure_time, self.rgb_sensor_iso)
+            camera.initialControl.setManualWhiteBalance(self.rgb_wb_manual)
+        else:
+            camera.initialControl.SceneMode(dai.CameraControl.SceneMode.SPORTS)
+            camera.initialControl.setAutoWhiteBalanceMode(dai.CameraControl.AutoWhiteBalanceMode.AUTO)
+
+        # Define output and link
+        xout = self.pipeline.create(dai.node.XLinkOut)
+        xout.setStreamName("xout")
+        camera.preview.link(xout.input)
+
+    def create_mono_pipeline(self):
+        # Source
+        camera = self.pipeline.create(dai.node.MonoCamera)
+        camera.setFps(self.framerate)
+        camera.setBoardSocket(dai.CameraBoardSocket.LEFT)
+        camera.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+
+        # Resize image
+        manip = self.pipeline.create(dai.node.ImageManip)
+        manip.setMaxOutputFrameSize(self.width * self.height)
+        manip.initialConfig.setResize(self.width, self.height)
+        manip.initialConfig.setFrameType(dai.RawImgFrame.Type.GRAY8)
+
+        # Define output and link
+        xout = self.pipeline.create(dai.node.XLinkOut)
+        xout.setStreamName("xout")
+        camera.out.link(manip.inputImage)
+        manip.out.link(xout.input)
+
+    def create_depth_pipeline(self):
         # Create depth nodes
         monoRight = self.pipeline.create(dai.node.MonoCamera)
         monoLeft = self.pipeline.create(dai.node.MonoCamera)
         stereo_manip = self.pipeline.create(dai.node.ImageManip)
         stereo = self.pipeline.create(dai.node.StereoDepth)
 
-        # Better handling for occlusions:
-        stereo.setLeftRightCheck(True)
-        # Closer-in minimum depth, disparity range is doubled:
-        stereo.setExtendedDisparity(True)
-        # Better accuracy for longer distance, fractional disparity 32-levels:
-        stereo.setSubpixel(False)
+        stereo.setLeftRightCheck(self.lr_check)
+        stereo.setExtendedDisparity(self.extended_disparity)
+        stereo.setSubpixel(self.subpixel)
         stereo.initialConfig.setMedianFilter(dai.MedianFilter.KERNEL_7x7)
         stereo.initialConfig.setConfidenceThreshold(200)
 
@@ -335,7 +313,6 @@ class OakDCamera:
         stereo_manip.out.link(xout_depth.input)
 
     def create_obstacle_dist_pipeline(self):
-
         # Define sources and outputs
         monoLeft = self.pipeline.create(dai.node.MonoCamera)
         monoRight = self.pipeline.create(dai.node.MonoCamera)
@@ -357,8 +334,9 @@ class OakDCamera:
         monoRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
 
         stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
-        stereo.setLeftRightCheck(True)
-        stereo.setExtendedDisparity(True)
+        stereo.setLeftRightCheck(self.lr_check)
+        stereo.setExtendedDisparity(self.extended_disparity)
+        stereo.setSubpixel(self.subpixel)
         spatialLocationCalculator.inputConfig.setWaitForMessage(False)
 
         for i in range(4):
@@ -382,18 +360,21 @@ class OakDCamera:
         spatialLocationCalculator.out.link(xoutSpatialData.input)
         xinSpatialCalcConfig.out.link(spatialLocationCalculator.inputConfig)
 
-
     def run(self):
-
         # Grab the frame from the stream 
         if self.queue_xout is not None:
             data_xout = self.queue_xout.get() # blocking
             image_data_xout = data_xout.getFrame()
-            self.frame_xout = np.moveaxis(image_data_xout,0,-1)
+            if self.depth == 3:
+                image_data_xout = np.moveaxis(image_data_xout,0,-1)
+            self.frame_xout = image_data_xout
+
+            latency = (dai.Clock.now() - data_xout.getTimestamp()).total_seconds()
+            self.frame_time = time.time() - latency
 
             if logger.isEnabledFor(logging.DEBUG):
                 # Latency in miliseconds 
-                self.latencies.append((dai.Clock.now() - data_xout.getTimestamp()).total_seconds() * 1000)
+                self.latencies.append(latency * 1000)
                 if len(self.latencies) >= self.latencies.maxlen:
                     logger.debug('Image latency: {:.2f} ms, Average latency: {:.2f} ms, Std: {:.2f}' \
                         .format(self.latencies[-1], np.average(self.latencies), np.std(self.latencies)))
@@ -408,12 +389,6 @@ class OakDCamera:
             self.roi_distances = []
             for depthData in xout_spatial_data:
                 roi = depthData.config.roi
-                
-                # xmin = int(roi.topLeft().x)
-                # ymin = int(roi.topLeft().y)
-                # xmax = int(roi.bottomRight().x)
-                # ymax = int(roi.bottomRight().y)
-
                 coords = depthData.spatialCoordinates
                 
                 self.roi_distances.append(round(roi.topLeft().x,2)) 
@@ -428,7 +403,7 @@ class OakDCamera:
 
     def run_threaded(self):
         if self.enable_depth:
-            return self.frame_xout,self.frame_xout_depth
+            return self.frame_xout, self.frame_xout_depth
         elif self.enable_obstacle_dist:
             return self.frame_xout, np.array(self.roi_distances)
         else:
