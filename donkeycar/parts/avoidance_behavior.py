270691af76afc18c0ceada41b438b801b36d94ce
btrinite <btrinite@free.fr>
2023-11-14 14:34:30 +0100

    fix

--- donkeycar/parts/avoidance_behavior.py
+++ donkeycar/parts/avoidance_behavior.py
@@ -26,6 +26,16 @@ class AvoidanceBehaviorPart(object):
     def run(self, detector_obstacle_lane):
         one_hot_bhv_arr = np.zeros(self.lane_options_size)
 
+        if (self.manual_lane):
+            #For test purpose, will follow a lane given from remote controller
+            lane=self.hatInCtrl.getSelectedLane()
+            if lane==0:
+                one_hot_bhv_arr[self.lane_behavior_left_index] = 1.0
+            elif lane==2:
+                one_hot_bhv_arr[self.lane_behavior_right_index] = 1.0
+            else:
+                one_hot_bhv_arr[self.lane_behavior_middle_index] = 1.0
+
         if self.avoidance_enabled:
             # get text value from obstacle position
             obstacle_position_text = self.obstacle_states[detector_obstacle_lane]
@@ -33,23 +43,12 @@ class AvoidanceBehaviorPart(object):
             # get desired behavior regarding obstacle position
             # search index of desired behavior
             # create one_hot_bhv_arr with desired behavior
-
-            if (self.manual_lane):
-                #For test purpose, will follow a lane given from remote controller
-                lane=self.hatInCtrl.getSelectedLane()
-                if lane==0:
-                    one_hot_bhv_arr[self.lane_behavior_left_index] = 1.0
-                elif lane==2:
-                    one_hot_bhv_arr[self.lane_behavior_right_index] = 1.0
-                else:
-                    one_hot_bhv_arr[self.lane_behavior_middle_index] = 1.0
-            else:
-                if obstacle_position_text == "left":
-                    one_hot_bhv_arr[self.lane_behavior_right_index] = 1.0
-                elif obstacle_position_text == "right":
-                    one_hot_bhv_arr[self.lane_behavior_left_index] = 1.0
-                elif obstacle_position_text == "middle":
-                    one_hot_bhv_arr[self.lane_behavior_middle_index] = 1.0
+            if obstacle_position_text == "left":
+                one_hot_bhv_arr[self.lane_behavior_right_index] = 1.0
+            elif obstacle_position_text == "right":
+                one_hot_bhv_arr[self.lane_behavior_left_index] = 1.0
+            elif obstacle_position_text == "middle":
+                one_hot_bhv_arr[self.lane_behavior_left_index] = 1.0
             # elif obstacle_position_text == "NA":
                 # SET TO 0.0 WHEN MODEL CAN HANDLE [0.0,0.0], NO lane seletion = regular driving
                 # at the moment default driving is left lane driving
