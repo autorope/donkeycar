6b96b770b5fff0d9ceb95b95063554417db74fe5
DocGarbanzo <dirk.prange@web.de>
2024-03-14 22:35:15 +0000

    Required changes for updating to py 3.11 and tf 2.15 / 2.13

--- donkeycar/tests/test_telemetry.py
+++ donkeycar/tests/test_telemetry.py
@@ -3,6 +3,8 @@
 import time
 from unittest import mock
 from paho.mqtt.client import Client
+from paho.mqtt.enums import CallbackAPIVersion
+
 import donkeycar.templates.cfg_complete as cfg
 from donkeycar.parts.telemetry import MqttTelemetry
 import pytest
@@ -16,7 +18,8 @@ def test_mqtt_telemetry():
     cfg.TELEMETRY_MQTT_JSON_ENABLE = True
 
     # Create receiver
-    sub = Client(clean_session=True)
+    sub = Client(callback_api_version=CallbackAPIVersion.API_VERSION2,
+                 clean_session=True)
 
     # def on_message(client, userdata, message):
     #     data = message.payload
