079ecaf7d6e836ac92c58ea80a1a0146576c43a7
DocGarbanzo <dirk.prange@web.de>
2024-05-21 21:32:55 +0100

    * skip augmentations when list is empty * replacye spurios __file__ in load_config with os.getcwd() * pin matplotlib versions to 3.8.* * fix log statement in training

--- donkeycar/config.py
+++ donkeycar/config.py
@@ -66,8 +66,7 @@ class Config:
 def load_config(config_path=None, myconfig="myconfig.py"):
     
     if config_path is None:
-        import __main__ as main
-        main_path = os.path.dirname(os.path.realpath(main.__file__))
+        main_path = os.getcwd()
         config_path = os.path.join(main_path, 'config.py')
         if not os.path.exists(config_path):
             local_config = os.path.join(os.path.curdir, 'config.py')
