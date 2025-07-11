6ec64a262e0c322d083bb7294d90a1fb2e4e52ad
Manav Gagvani <mgagvani@outlook.com>
2025-07-11 02:04:05 +0000

    address review, black-ed everything, bump version

--- donkeycar/__init__.py
+++ donkeycar/__init__.py
@@ -3,7 +3,7 @@ import sys
 from pyfiglet import Figlet
 import logging
 
-__version__ = '5.2.dev3'
+__version__ = '5.2.dev4'
 
 logging.basicConfig(level=os.environ.get('LOGLEVEL', 'INFO').upper())
 
--- donkeycar/parts/oak_d.py
+++ donkeycar/parts/oak_d.py
@@ -1,7 +1,7 @@
 """
 Author: Brian Henry & Manav Gagvani
 File: oak_d.py
-Date: February 13 2022, revised January 31, 2024
+Date: February 13 2022, revised July 10, 2025
 Notes:
     Based on realsense435i.py by Ed Murphy: https://github.com/autorope/donkeycar/blob/454be3068ea5dfbac226c3be4d84b0a61d1cec84/donkeycar/parts/realsense435i.py
     Based on https://github.com/luxonis/depthai-tutorials/blob/d571473911f876b0d4ac52b7ffdc0fb2beae1641/1-hello-world/hello_world.py
@@ -14,6 +14,7 @@ Notes:
 
     `sudo pip3 install --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v461 tensorflow`
 """
+
 import argparse
 import string
 import time
@@ -39,7 +40,14 @@ class OakD(object):
     https://shop.luxonis.com/
     """
 
-    def __init__(self, width=WIDTH, height=HEIGHT, enable_rgb=True, enable_depth=True, device_id=None):
+    def __init__(
+        self,
+        width=WIDTH,
+        height=HEIGHT,
+        enable_rgb=True,
+        enable_depth=True,
+        device_id=None,
+    ):
         self.device_id = device_id  # "18443010C1E4681200" # serial number of device to use|None to use default|"list" to list devices and exit
         self.enable_rgb = enable_rgb
         self.enable_depth = enable_depth
@@ -51,8 +59,7 @@ class OakD(object):
         self.resize = (width != WIDTH) or (height != height)
         if self.resize:
             print(
-                "The output images will be resized from {} to {} using OpenCV. Device resolution in use is 640x480.".format(
-                    (WIDTH, HEIGHT), (self.width, self.height))
+                f"The output images will be resized from {(WIDTH, HEIGHT)} to {(self.width, self.height)} using OpenCV. Device resolution in use is 640x480."
             )
 
         self.pipeline = None
@@ -92,9 +99,13 @@ class OakD(object):
             if device_id == "list":
                 raise SystemExit(0)
             elif device_id is not None:
-                matching_device = next(filter(lambda info: info.getMxId() == device_id, device_infos), None)
+                matching_device = next(
+                    filter(lambda info: info.getMxId() == device_id, device_infos), None
+                )
                 if matching_device is None:
-                    raise RuntimeError(f"No DepthAI device found with id matching {device_id} !")
+                    raise RuntimeError(
+                        f"No DepthAI device found with id matching {device_id} !"
+                    )
                 return matching_device
             elif len(device_infos) == 1:
                 return device_infos[0]
@@ -103,7 +114,7 @@ class OakD(object):
                 try:
                     return device_infos[int(val)]
                 except:
-                    raise ValueError("Incorrect value supplied: {}".format(val))
+                    raise ValueError(f"Incorrect value supplied: {val}")
 
     def setup_depth_camera(self, width, height):
         # Set up left and right cameras
@@ -111,7 +122,9 @@ class OakD(object):
         mono_right = self.get_mono_camera(self.pipeline, False)
 
         # Combine left and right cameras to form a stereo pair
-        stereo: depthai.node.StereoDepth = self.get_stereo_pair(self.pipeline, mono_left, mono_right)
+        stereo: depthai.node.StereoDepth = self.get_stereo_pair(
+            self.pipeline, mono_left, mono_right
+        )
 
         # Define and name output depth map
         xout_depth = self.pipeline.createXLinkOut()
@@ -125,13 +138,7 @@ class OakD(object):
         res = depthai.ColorCameraProperties.SensorResolution.THE_1080_P
 
         cam_rgb.setResolution(res)
-        # cam_rgb.setInterleaved(False)
         cam_rgb.setVideoSize(width, height)
-        # 1080 -> 480
-        # *
-        # cam_rgb.setIspScale(480, 1080)
-        # cam_rgb.set3
-        # cam_rgb.setFps(22)
 
         xout_rgb = self.pipeline.create(depthai.node.XLinkOut)
         xout_rgb.setStreamName("rgb")
@@ -139,20 +146,20 @@ class OakD(object):
         cam_rgb.video.link(xout_rgb.input)
 
     def get_mono_camera(self, pipeline: Pipeline, is_left: bool):
-            # Configure mono camera
-            mono = pipeline.createMonoCamera()
+        # Configure mono camera
+        mono = pipeline.createMonoCamera()
 
-            # Set camera resolution
-            mono.setResolution(depthai.MonoCameraProperties.SensorResolution.THE_480_P)
+        # Set camera resolution
+        mono.setResolution(depthai.MonoCameraProperties.SensorResolution.THE_480_P)
 
-            if is_left:
-                # Get left camera
-                mono.setBoardSocket(depthai.CameraBoardSocket.LEFT)
-            else:
-                # Get right camera
-                mono.setBoardSocket(depthai.CameraBoardSocket.RIGHT)
+        if is_left:
+            # Get left camera
+            mono.setBoardSocket(depthai.CameraBoardSocket.LEFT)
+        else:
+            # Get right camera
+            mono.setBoardSocket(depthai.CameraBoardSocket.RIGHT)
 
-            return mono
+        return mono
 
     def get_stereo_pair(self, pipeline: Pipeline, mono_left, mono_right):
         # Configure the stereo pair for depth estimation
@@ -182,9 +189,12 @@ class OakD(object):
         #
         if self.enable_rgb or self.enable_depth:
 
-            self.depth_queue: DataOutputQueue = self.oak_d_device.getOutputQueue(name="depth", maxSize=1,
-                                                                                 blocking=False)
-            self.rgb_queue: DataOutputQueue = self.oak_d_device.getOutputQueue("rgb", maxSize=1, blocking=False)
+            self.depth_queue: DataOutputQueue = self.oak_d_device.getOutputQueue(
+                name="depth", maxSize=1, blocking=False
+            )
+            self.rgb_queue: DataOutputQueue = self.oak_d_device.getOutputQueue(
+                "rgb", maxSize=1, blocking=False
+            )
 
             depth_frame = self.get_frame(self.depth_queue)
             rgb_frame = self.get_frame(self.rgb_queue)
@@ -193,12 +203,23 @@ class OakD(object):
             self.color_image = rgb_frame
 
         if self.resize:
-                if self.width != WIDTH or self.height != HEIGHT:
-                    import cv2
-                    self.color_image = cv2.resize(self.color_image, (self.width, self.height),
-                                                  cv2.INTER_NEAREST) if self.enable_rgb else None
-                    self.depth_image = cv2.resize(self.depth_image, (self.width, self.height),
-                                                  cv2.INTER_NEAREST) if self.enable_depth else None
+            if self.width != WIDTH or self.height != HEIGHT:
+                import cv2
+
+                self.color_image = (
+                    cv2.resize(
+                        self.color_image, (self.width, self.height), cv2.INTER_NEAREST
+                    )
+                    if self.enable_rgb
+                    else None
+                )
+                self.depth_image = (
+                    cv2.resize(
+                        self.depth_image, (self.width, self.height), cv2.INTER_NEAREST
+                    )
+                    if self.enable_depth
+                    else None
+                )
 
     def update(self):
         """
@@ -240,9 +261,16 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--rgb", default=False, action='store_true', help="Stream RGB camera")
-    parser.add_argument("--depth", default=False, action='store_true', help="Stream depth camera")
-    parser.add_argument("--device_id", help="Camera id (if more than one camera connected), or \"list\" to print the connected device ids")
+    parser.add_argument(
+        "--rgb", default=False, action="store_true", help="Stream RGB camera"
+    )
+    parser.add_argument(
+        "--depth", default=False, action="store_true", help="Stream depth camera"
+    )
+    parser.add_argument(
+        "--device_id",
+        help='Camera id (if more than one camera connected), or "list" to print the connected device ids',
+    )
     args = parser.parse_args()
 
     if not (args.rgb or args.depth):
@@ -250,7 +278,9 @@ if __name__ == "__main__":
         parser.print_help()
         sys.exit(0)
 
-    show_opencv_window = args.rgb or args.depth  # True to show images in opencv window: note that default donkeycar environment is not configured for this.
+    show_opencv_window = (
+        args.rgb or args.depth
+    )  # True to show images in opencv window: note that default donkeycar environment is not configured for this.
     if show_opencv_window:
         import cv2
 
@@ -259,7 +289,7 @@ if __name__ == "__main__":
 
     devices = depthai.Device.getAllAvailableDevices()
 
-    device_id = args.device_id # getMxId
+    device_id = args.device_id  # getMxId
 
     width = 640
     height = 480
@@ -270,8 +300,12 @@ if __name__ == "__main__":
     camera = None
     try:
         camera = OakDLite(
-            width=width, height=height,
-            enable_rgb=enable_rgb, enable_depth=enable_depth, device_id=device_id)
+            width=width,
+            height=height,
+            enable_rgb=enable_rgb,
+            enable_depth=enable_depth,
+            device_id=device_id,
+        )
 
         frame_count = 0
         start_time = time.time()
@@ -289,38 +323,56 @@ if __name__ == "__main__":
 
             # Show images
             if show_opencv_window and not profile_frames:
-                cv2.namedWindow('Oak-D', cv2.WINDOW_AUTOSIZE)
+                cv2.namedWindow("Oak-D", cv2.WINDOW_AUTOSIZE)
                 if enable_rgb or enable_depth:
                     # make sure depth and color images have same number of channels so we can show them together in the window
                     if 3 == channels:
-                        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03),
-                                                           cv2.COLORMAP_JET) if enable_depth else None
+                        depth_colormap = (
+                            cv2.applyColorMap(
+                                cv2.convertScaleAbs(depth_image, alpha=0.03),
+                                cv2.COLORMAP_JET,
+                            )
+                            if enable_depth
+                            else None
+                        )
                     else:
-                        depth_colormap = cv2.cvtColor(
-                            cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET),
-                            cv2.COLOR_RGB2GRAY) if enable_depth else None
+                        depth_colormap = (
+                            cv2.cvtColor(
+                                cv2.applyColorMap(
+                                    cv2.convertScaleAbs(depth_image, alpha=0.03),
+                                    cv2.COLORMAP_JET,
+                                ),
+                                cv2.COLOR_RGB2GRAY,
+                            )
+                            if enable_depth
+                            else None
+                        )
 
                     # Stack both images horizontally (i.e. side by side).
                     images = None
                     if enable_rgb:
-                        images = np.hstack((color_image, depth_colormap)) if enable_depth else color_image
+                        images = (
+                            np.hstack((color_image, depth_colormap))
+                            if enable_depth
+                            else color_image
+                        )
                     elif enable_depth:
                         images = depth_colormap
 
                     if images is not None:
-                        cv2.imshow('Oak-D', images)
+                        cv2.imshow("Oak-D", images)
 
                 # Press esc or 'q' to close the image window
                 key = cv2.waitKey(1)
-                if key & 0xFF == ord('q') or key == 27:
+                if key & 0xFF == ord("q") or key == 27:
                     cv2.destroyAllWindows()
                     break
             if profile_frames > 0:
                 if frame_count == profile_frames:
-                    print("Aquired {} frames in {} seconds for {} fps".format(str(frame_count),
-                                                                              str(frame_time - start_time),
-                                                                              str(frame_count / (
-                                                                                          frame_time - start_time))))
+                    print(
+                        f"Acquired {frame_count} frames in {frame_time - start_time} seconds for {frame_count / (frame_time - start_time)} fps"
+                    )
+
                     break
             else:
                 time.sleep(0.05)
